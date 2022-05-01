37e341d07ecf4bad5743bdebc5d1647b3bacdaba
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-04-30 20:13:09 -0700

    Create overfitting.js

---
+++ tasks/overfitting.js
@@ -0,0 +1,7 @@
+"what": [
+      "Creating a model that matches the training data so closely that the model fails to make correct predictions on new data.",
+      "[motivation] to understand the cause",
+      "[related] model, training data, fails to make correct predictions, new data, [motivation] cause",
+      "[関連] 模型 [japanese pronunciation], 研修資料  [japanese pronunciation], 失敗 [japanese pronunciation], 予測 [japanese pronunciation], 新データ [japanese pronunciation], [動機] 原因 [japanese pronunciation]",	    
+  
+    ],
