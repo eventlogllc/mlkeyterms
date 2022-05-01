418723585b94706e17379dd9d9918b1a662404bf
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-05-01 00:16:08 -0700

    Create model.js

---
+++ tasks/model.js
@@ -0,0 +1,6 @@
+    "what": [
+      "The representation of what a machine learning system has learned from the training data.",
+      "A statistical model is a mathematical model that embodies a set of statistical assumptions concerning the generation of sample data (and similar data from a larger population). A statistical model represents, often in considerably idealized form, the data-generating process. A statistical model is usually specified as a mathematical relationship between one or more random variables and other non-random variables. As such, a statistical model is 'a formal representation of a theory'", 
+      "All statistical hypothesis tests and all statistical estimators are derived via statistical models. More generally, statistical models are part of the foundation of statistical inference.",
+      "A mathematical model is a description of a system using mathematical concepts and language. The process of developing a mathematical model is termed mathematical modeling. Mathematical models are used in the natural sciences (such as physics, biology, earth science, chemistry) and engineering disciplines (such as computer science, electrical engineering), as well as in non-physical systems such as the social sciences (such as economics, psychology, sociology, political science)",
+    ],
