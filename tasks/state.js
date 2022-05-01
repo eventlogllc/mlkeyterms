02760367c337d0141bc0e6104d70bbda3f349ce2
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-05-01 00:28:54 -0700

    Update state.js

--- tasks/state.js
+++ tasks/state.js
@@ -1,3 +1,8 @@
 "what": [
-      "In reinforcement learning, the parameter values that describe the current configuration of the environment, which the agent uses to choose an action."
+      "In reinforcement learning, the parameter values that describe the current configuration of the environment, which the agent uses to choose an action.",
+      "[motivation]",
+      "[related] reinforcement learning, parameter values, describe, current configuration, environment, agent, choose an action",
+      "[関連] 強化学習 [japanese pronunciation], パラメータ値  [japanese pronunciation],説明 [japanese pronunciation], 現在の構成 [japanese pronunciation], 環境 [japanese pronunciat, ion], エージェント [japanese pronunciation], アクションを選択 [japanese pronunciation]",	    
+      "[相關] 強化學習、參數值, 描述, 當前配置, 環境，個體，選擇一個動作",
     ],
+ 
