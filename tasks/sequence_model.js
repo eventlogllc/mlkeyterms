9e06512977be7a7300017b34cb0bb0857f4d1ee9
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-05-05 18:00:41 -0700

    Update sequence_model.js

--- tasks/sequence_model.js
+++ tasks/sequence_model.js
@@ -1,6 +1,6 @@
     "what": [
       "A model whose inputs have a sequential dependence. For example, predicting the next video watched from a sequence of previously watched videos.",
       "[related] model, inputs, probabilistic, for example, next video, from, a sequence of, previously watched videos",
-      "[関連] 強化学習 [japanese pronunciation], エージェント [japanese pronunciation], 確率的 [japanese pronunciation], マッピング [japanese pronunciation],状態 [japanese pronunciation],アクション [japanese pronunciation]",	    
+      "[関連] モデル, 入力, 確率論的, たとえば, 次のビデオ, 一連の, 以前に視聴したビデオ [japanese pronunciation]",	    
       "[相關] 模型, 輸入, 概率, 例如, 下一個視頻, 來自, 一系列以前觀看過的視頻",  
     ],
