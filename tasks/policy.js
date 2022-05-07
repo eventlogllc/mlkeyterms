5a5e8f58f54204835e6c837d1014a15099315cc7
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-05-07 14:25:06 -0700

    Update policy.js

--- tasks/policy.js
+++ tasks/policy.js
@@ -1,6 +1,6 @@
     "what": [
       "In reinforcement learning, an agent's probabilistic mapping from states to actions.",
-      "[related] reinforcement learning, agent, probabilistic, mapping, states, actions",
-      "[関連] 強化学習 [pronunciation] [hiragana or katakana], エージェント [eijento][えーじぇんと], 確率的 [pronunciation] [hiragana or katakana], マッピング [pronunciation] [hiragana or katakana],状態 [pronunciation] [hiragana or katakana],アクション [pronunciation] [hiragana or katakana]",	    
-      "[相關] 強化學習,代理,概率,映射,狀態,動作",
+      "[英] [related] reinforcement learning, agent, probabilistic, mapping, states, actions",
+      "[和] [関連] 強化学習 [pronunciation] [hiragana or katakana], エージェント [eijento][えーじぇんと], 確率的 [pronunciation] [hiragana or katakana], マッピング [pronunciation] [hiragana or katakana],状態 [pronunciation] [hiragana or katakana],アクション [pronunciation] [hiragana or katakana]",	    
+      "[漢] [相關] 強化學習,代理,概率,映射,狀態,動作",
     ],
