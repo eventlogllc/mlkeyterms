c83b9e0f878ac1ed75751d4bffb142afa646c77d
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-05-07 14:21:35 -0700

    Update Deep_Q_Network__DQN_.js

--- tasks/Deep_Q_Network__DQN_.js
+++ tasks/Deep_Q_Network__DQN_.js
@@ -1,6 +1,6 @@
     "what": [
       "In Q-learning, a deep neural network that predicts Q-functions.",
-      "[related] Q-learning, a deep neural network, predicts, Q-functions",
-      "[関連] Q-学習 [pronunciation] [hiragana or katakana], 深いニューラルネットワーク [pronunciation] [hiragana or katakana], 予測 [pronunciation] [hiragana or katakana], Q-関数 [pronunciation] [hiragana or katakana]",	    
-      "[相關] Q-學習，深度神經網絡，預測，Q-函數",
+      "[英] [related] Q-learning, a deep neural network, predicts, Q-functions",
+      "[和] [関連] Q-学習 [pronunciation] [hiragana or katakana], 深いニューラルネットワーク [pronunciation] [hiragana or katakana], 予測 [pronunciation] [hiragana or katakana], Q-関数 [pronunciation] [hiragana or katakana]",	    
+      "[漢] Q-學習，深度神經網絡，預測，Q-函數",
     ],
