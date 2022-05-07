50b9a3693e7d002a6760bf8f2075cd87490736e8
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-05-07 14:21:44 -0700

    Update sequence_model.js

--- tasks/sequence_model.js
+++ tasks/sequence_model.js
@@ -1,6 +1,6 @@
     "what": [
       "A model whose inputs have a sequential dependence. For example, predicting the next video watched from a sequence of previously watched videos.",
-      "[英][related] model, inputs, probabilistic, for example, next video, from, a sequence of, previously watched videos",
-      "[和][関連] モデル [pronunciation] [hiragana or katakana], 入力 [pronunciation] [hiragana or katakana], 確率的 [pronunciation] [hiragana or katakana], たとえば [pronunciation] [hiragana or katakana], 次のビデオ [pronunciation] [hiragana or katakana], 一連の [pronunciation] [hiragana or katakana], 以前に視聴したビデオ [pronunciation] [hiragana or katakana]",	    
-      "[漢][相關] 模型, 輸入, 概率, 例如, 下一個視頻, 來自, 一系列以前觀看過的視頻",  
+      "[英] [related] model, inputs, probabilistic, for example, next video, from, a sequence of, previously watched videos",
+      "[和] [関連] モデル [pronunciation] [hiragana or katakana], 入力 [pronunciation] [hiragana or katakana], 確率的 [pronunciation] [hiragana or katakana], たとえば [pronunciation] [hiragana or katakana], 次のビデオ [pronunciation] [hiragana or katakana], 一連の [pronunciation] [hiragana or katakana], 以前に視聴したビデオ [pronunciation] [hiragana or katakana]",	    
+      "[漢] [相關] 模型, 輸入, 概率, 例如, 下一個視頻, 來自, 一系列以前觀看過的視頻",  
     ],
