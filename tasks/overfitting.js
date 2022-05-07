9d5f5cd7243062f3b04f155893f28462dd535aa2
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-05-07 14:24:30 -0700

    Update overfitting.js

--- tasks/overfitting.js
+++ tasks/overfitting.js
@@ -1,7 +1,7 @@
 "what": [
       "Creating a model that matches the training data so closely that the model fails to make correct predictions on new data.",
       "[motivation] to understand the cause",
-      "[related] model, training data, fails to make correct predictions, new data, [motivation] cause",
-      "[関連] 模型 [mokei] [もけい], 研修資料  [pronunciation] [hiragana or katakana], 失敗 [pronunciation] [hiragana or katakana], 予測 [pronunciation] [hiragana or katakana], 新データ [pronunciation] [hiragana or katakana], [動機] 原因 [pronunciation] [hiragana or katakana]",	    
-      "[相關] 模型、培訓材料、失敗、預測、新數據、[動機] 原因",
+      "[英] [related] model, training data, fails to make correct predictions, new data, [motivation] cause",
+      "[和] [関連] 模型 [mokei] [もけい], 研修資料  [pronunciation] [hiragana or katakana], 失敗 [pronunciation] [hiragana or katakana], 予測 [pronunciation] [hiragana or katakana], 新データ [pronunciation] [hiragana or katakana], [動機] 原因 [pronunciation] [hiragana or katakana]",	    
+      "[漢] [相關] 模型、培訓材料、失敗、預測、新數據、[動機] 原因",
     ],
