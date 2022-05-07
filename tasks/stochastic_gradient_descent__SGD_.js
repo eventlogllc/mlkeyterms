d425c647c3fd5be1d534bef6d8d41fe63abb7562
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-05-06 19:13:53 -0700

    Update stochastic_gradient_descent__SGD_.js

--- tasks/stochastic_gradient_descent__SGD_.js
+++ tasks/stochastic_gradient_descent__SGD_.js
@@ -1,3 +1,6 @@
     "what": [
-      "A gradient descent algorithm in which the batch size is one. In other words, SGD relies on a single example chosen uniformly at random from a dataset to calculate an estimate of the gradient at each step."
+      "A gradient descent algorithm in which the batch size is one. In other words, SGD relies on a single example chosen uniformly at random from a dataset to calculate an estimate of the gradient at each step.",
+      "[related] gradient, descent, algorithm, batch size, one, relies on, single example, chosen, uniformly, random, from, dataset, to calculate, estimate, gradient, at each step.",
+      "[関連] 強化学習 [pronunciation] [hiragana or katakana], パラメータ値 [pronunciation] [hiragana or katakana], 説明 [pronunciation] [hiragana or katakana], 現在の構成 [pronunciation] [hiragana or katakana], 環境 [pronunciation] [hiragana or katakana], エージェント [pronunciation] [hiragana or katakana], アクションを選択 [pronunciation] [hiragana or katakana]",	    
+      "[相關] 強化學習、參數值, 描述, 當前配置, 環境，個體，選擇一個動作",  
     ],
