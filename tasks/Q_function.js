f550bf00802d2c493348909b1fd72d1bd25cc970
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-05-06 19:26:10 -0700

    Update Q_function.js

--- tasks/Q_function.js
+++ tasks/Q_function.js
@@ -3,5 +3,5 @@
       "Q-function is also known as state-action value function.",
       "[related] reinforcement learning, the function, predicts, expected return, from, taking, action, in a state, then following, a given policy, state-action, value function.",
       "[関連] 強化学習 [pronunciation] [hiragana or katakana], 関数 [pronunciation] [hiragana or katakana], 予測 [pronunciation] [hiragana or katakana], 期待収益 [pronunciation] [hiragana or katakana], から [pronunciation] [hiragana or katakana], 実行 [pronunciation] [hiragana or katakana], アクション [pronunciation] [hiragana or katakana], 状態で [pronunciation] [hiragana or katakana], その後 [pronunciation] [hiragana or katakana], 特定のポリシー [pronunciation] [hiragana or katakana], 状態アクション [pronunciation] [hiragana or katakana], 値関数 [pronunciation] [hiragana or katakana]",	    
-      "[相關] 強化學習,代理,概率,映射,狀態,動作",
+      "[相關] 強化學習，函數，預測，預期回報，從，採取，行動，處於一種狀態，然後遵循，給定的策略，狀態-行動，價值函數",
     ],
