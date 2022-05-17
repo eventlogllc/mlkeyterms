da68d8fc8e05a7a95c15fd5319d06c2903074d68
akiko-113 <104332806+akiko-113@users.noreply.github.com>
2022-05-17 18:05:24 +0900

    Update model.js

--- tasks/model.js
+++ tasks/model.js
@@ -4,6 +4,6 @@
       "All statistical hypothesis tests and all statistical estimators are derived via statistical models. More generally, statistical models are part of the foundation of statistical inference.",
       "A mathematical model is a description of a system using mathematical concepts and language. The process of developing a mathematical model is termed mathematical modeling. Mathematical models are used in the natural sciences (such as physics, biology, earth science, chemistry) and engineering disciplines (such as computer science, electrical engineering), as well as in non-physical systems such as the social sciences (such as economics, psychology, sociology, political science)",
       "[英] [related] representation, a machine learning system, learned, from, the training data.",
-      "[和] [関連] 表現 [pronunciation] [hiragana or katakana], 機械学習システム [pronunciation] [hiragana or katakana], 学習済み [pronunciation] [hiragana or katakana], から [pronunciation] [hiragana or katakana], からトレーニングデータ [pronunciation] [hiragana or katakana]",	    
+      "[和] [関連] 表現 [Hyougen] [ひょうげん], 機械学習システム [Kikai Gakushuu Systemu] [きかいがくしゅうシステム], 学習済み [Gakushuu zumi] [がくしゅうずみ], から [Kara] [から], トレーニングデータ [Toreiningu deeta] [トレーニングデータ]",	    
       "[漢] [相關] 表示，機器學習系統，學習，來自，訓練數據",  
     ],
