521c0a229904da90780dbbad77e9194878bfa5dc
akiko-113 <104332806+akiko-113@users.noreply.github.com>
2022-05-17 18:08:34 +0900

    Update overfitting.js

--- tasks/overfitting.js
+++ tasks/overfitting.js
@@ -2,6 +2,6 @@
       "Creating a model that matches the training data so closely that the model fails to make correct predictions on new data.",
       "[motivation] to understand the cause",
       "[英] [related] model, training data, fails to make correct predictions, new data, [motivation] cause",
-      "[和] [関連] 模型 [mokei] [もけい], 研修資料  [pronunciation] [hiragana or katakana], 失敗 [pronunciation] [hiragana or katakana], 予測 [pronunciation] [hiragana or katakana], 新データ [pronunciation] [hiragana or katakana], [動機] 原因 [pronunciation] [hiragana or katakana]",	    
+      "[和] [関連] モデル [moderu] [モデル], トレーニングデータ  [Toreiningu deeta] [トレーニングデータ], 失敗 [Shippai] [しっぱい], 予測 [Yosoku] [よそく], 新データ [Shin Deeta] [しんデータ], [動機] [Douki] [どうき] 原因 [Geninn] [げんいん]",	    
       "[漢] [相關] 模型、培訓材料、失敗、預測、新數據、[動機] 原因",
     ],
