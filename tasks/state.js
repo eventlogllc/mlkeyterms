a2ebf683c34774f176de1fdf4e056197d54b0cb8
akiko-113 <104332806+akiko-113@users.noreply.github.com>
2022-05-17 18:26:45 +0900

    Update state.js

--- tasks/state.js
+++ tasks/state.js
@@ -2,7 +2,7 @@
       "In reinforcement learning, the parameter values that describe the current configuration of the environment, which the agent uses to choose an action.",
       "[motivation]",
       "[英] [related] reinforcement learning, parameter values, describe, current configuration, environment, agent, choose an action",
-      "[和] [関連] 強化学習 [pronunciation] [hiragana or katakana], パラメータ値 [pronunciation] [hiragana or katakana], 説明 [pronunciation] [hiragana or katakana], 現在の構成 [pronunciation] [hiragana or katakana], 環境 [pronunciation] [hiragana or katakana], エージェント [pronunciation] [hiragana or katakana], アクションを選択 [pronunciation] [hiragana or katakana]",	    
+      "[和] [関連] 強化学習 [Kyouka Gakushu] [きょうかがくしゅう], パラメータの値 [Parameeta no atai] [パラメータのあたい], 説明 [Setsumei] [せつめい], 現在の構成 [Genzai no Kousei] [げんざいのこうせい], 環境 [Kankyou] [かんきょう], エージェント [eejento] [エージェント], アクションを選択 [Akushion wo sentaku] [アクションをせんたく]",	    
       "[漢] [相關] 強化學習、參數值, 描述, 當前配置, 環境，個體，選擇一個動作",
     ],
  
