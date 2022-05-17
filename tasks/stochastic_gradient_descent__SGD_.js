976edafabccd44672855219e2943d4eba07458ed
akiko-113 <104332806+akiko-113@users.noreply.github.com>
2022-05-17 18:34:28 +0900

    Update stochastic_gradient_descent__SGD_.js

--- tasks/stochastic_gradient_descent__SGD_.js
+++ tasks/stochastic_gradient_descent__SGD_.js
@@ -1,6 +1,7 @@
     "what": [
       "A gradient descent algorithm in which the batch size is one. In other words, SGD relies on a single example chosen uniformly at random from a dataset to calculate an estimate of the gradient at each step.",
       "[英] [related] gradient, descent, algorithm, batch size, one, relies on, single example, chosen, uniformly, random, from, dataset, to calculate, estimate, gradient, at each step.",
-      "[和] [関連] 勾配 [pronunciation] [hiragana or katakana], 降下 [pronunciation] [hiragana or katakana], アルゴリズム [pronunciation] [hiragana or katakana], バッチサイズ [pronunciation] [hiragana or katakana], 1つは [pronunciation] [hiragana or katakana], 各ステップで計算 [pronunciation] [hiragana or katakana], 推定 [pronunciation] [hiragana or katakana], 勾配を計算するために [pronunciation] [hiragana or katakana], データセットから [pronunciation] [hiragana or katakana], 選択された単一の例に依存します [pronunciation] [hiragana or katakana]",	    
+      "[和] [関連] 勾配 [Koubai] [こうばい], 降下 [Kouka] [こうか], アルゴリズム [Arugorizumu] [アルゴリズム], バッチサイズ [Bacchi saizu] [バッチサイズ], 1 [ichi] [いち], 各ステップで計算 [kaku suteppu de keisan]
+[かくステップでけいさん], 推定 [Suitei] [すいてい], 勾配を計算するために [Koubai wo Keisansuru tameni] [こうばいをけいさんするために], データセットから [Deeta setto kara] [データセットから], 選択された単一の例に依存します [Sentaku sareta tannitsu no reini izonshimasu] [せんたくされた　たんいつのれいに　いぞんします]",	    
       "[漢] [相關] 梯度, 下降, 算法, 批量大小, 一個, 依賴於單個示例, 選擇, 均勻, 隨機, 來自數據集, 在每一步計算, 估計, 梯度",  
     ],
