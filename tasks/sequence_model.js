8c59c4afa04b7b776338a31b55e9ce8b0956903d
akiko-113 <104332806+akiko-113@users.noreply.github.com>
2022-05-17 18:22:35 +0900

    Update sequence_model.js

--- tasks/sequence_model.js
+++ tasks/sequence_model.js
@@ -1,6 +1,6 @@
     "what": [
       "A model whose inputs have a sequential dependence. For example, predicting the next video watched from a sequence of previously watched videos.",
       "[英] [related] model, inputs, probabilistic, for example, next video, from, a sequence of, previously watched videos",
-      "[和] [関連] モデル [pronunciation] [hiragana or katakana], 入力 [pronunciation] [hiragana or katakana], 確率的 [pronunciation] [hiragana or katakana], たとえば [pronunciation] [hiragana or katakana], 次のビデオ [pronunciation] [hiragana or katakana], 一連の [pronunciation] [hiragana or katakana], 以前に視聴したビデオ [pronunciation] [hiragana or katakana]",	    
+      "[和] [関連] モデル [moderu] [モデル], 入力 [Nyuuryoku] [にゅうりょく], 確率的 [Kakuritsu teki] [かくりつてき], たとえば [Tatoeba] [たとえば], 次のビデオ [Tsugi no bideo] [つぎのビデオ], 一連の [Ichiren no] [いちれんの], 前に視聴したビデオ [Mae ni Shichoshita bideo] [まえにしちょうしたビデオ]",	    
       "[漢] [相關] 模型, 輸入, 概率, 例如, 下一個視頻, 來自, 一系列以前觀看過的視頻",  
     ],
