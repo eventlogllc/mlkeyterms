3e732244ab47fd018a3dad3d821b043d5a3424d2
akiko-113 <104332806+akiko-113@users.noreply.github.com>
2022-05-17 17:45:06 +0900

    Update Deep_Q_Network__DQN_.js

--- tasks/Deep_Q_Network__DQN_.js
+++ tasks/Deep_Q_Network__DQN_.js
@@ -1,6 +1,6 @@
     "what": [
       "In Q-learning, a deep neural network that predicts Q-functions.",
       "[英] [related] Q-learning, a deep neural network, predicts, Q-functions",
-      "[和] [関連] Q-学習 [pronunciation] [hiragana or katakana], 深いニューラルネットワーク [pronunciation] [hiragana or katakana], 予測 [pronunciation] [hiragana or katakana], Q-関数 [pronunciation] [hiragana or katakana]",	    
+      "[和] [関連] Q学習 [Q gakushu] [Q がくしゅう], ディープニューラルネットワーク [Deepu nyuraru netto waaku] [ディープニューラルネットワーク], 予測 [yosoku] [よそく], Q関数 [Q kansuu] [Q かんすう]",	    
       "[漢] Q-學習，深度神經網絡，預測，Q-函數",
     ],
