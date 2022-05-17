69c9fdc4725cf7f2b0722b287802debeef37bfb4
akiko-113 <104332806+akiko-113@users.noreply.github.com>
2022-05-17 18:02:26 +0900

    Update Q_function.js

--- tasks/Q_function.js
+++ tasks/Q_function.js
@@ -2,6 +2,6 @@
       "In reinforcement learning, the function that predicts the expected return from taking an action in a state and then following a given policy.",
       "Q-function is also known as state-action value function.",
       "[英] [related] reinforcement learning, the function, predicts, expected return, from, taking, action, in a state, then following, a given policy, state-action, value function.",
-      "[和] [関連] 強化学習 [pronunciation] [hiragana or katakana], 関数 [pronunciation] [hiragana or katakana], 予測 [pronunciation] [hiragana or katakana], 期待収益 [pronunciation] [hiragana or katakana], から [pronunciation] [hiragana or katakana], 実行 [pronunciation] [hiragana or katakana], アクション [pronunciation] [hiragana or katakana], 状態で [pronunciation] [hiragana or katakana], その後 [pronunciation] [hiragana or katakana], 特定のポリシー [pronunciation] [hiragana or katakana], 状態アクション [pronunciation] [hiragana or katakana], 値関数 [pronunciation] [hiragana or katakana]",	    
+      "[和] [関連] 強化学習 [Kyouka Gakushu] [きょうかがくしゅう], 関数 [Kansuu] [かんすう], 予測 [Yosoku] [よそく], 期待収益 [Kitai Shuueki] [きたいしゅうえき], から [Kara] [から], 実行 [pronunciation] [hiragana or katakana], アクション [Akushion] [アクション], 状態で [Joutai de] [じょうたいで], その後 [Sonogo] [そのご], 規定の方策 [Kitei no Housaku] [きていのほうさく], 状態行動 [Joutai Koudou] [じょうたいこうどう], 価値関数 [Kachi Kansuu] [かちかんすう]",	    
       "[漢] [相關] 強化學習，函數，預測，預期回報，從，採取，行動，處於一種狀態，然後遵循，給定的策略，狀態-行動，價值函數",
     ],
