5ce9f0259f4bb06d0b13e651f39d2f32fba34f55
akiko-113 <104332806+akiko-113@users.noreply.github.com>
2022-05-18 10:29:26 +0900

    Update Deep_Q_Network__DQN_.js

--- tasks/Deep_Q_Network__DQN_.js
+++ tasks/Deep_Q_Network__DQN_.js
@@ -4,3 +4,8 @@
       "[和] [関連] Q学習 [Q gakushu] [Q がくしゅう], ディープニューラルネットワーク [Deepu nyuraru netto waaku] [ディープニューラルネットワーク], 予測 [yosoku] [よそく], Q関数 [Q kansuu] [Q かんすう]",	    
       "[漢] Q-學習，深度神經網絡，預測，Q-函數",
     ],
+
+        Q-learning: Q学習 [Q gakushu] [Q がくしゅう]: Q-學習
+        a deep neural network: ディープニューラルネットワーク [Deepu nyuraru netto waaku] [ディープニューラルネットワーク]: 深度神經網絡
+        predicts: 予測 [yosoku] [よそく]: 預測
+        Q-functions: Q関数 [Q kansuu] [Q かんすう]: Q-函數
