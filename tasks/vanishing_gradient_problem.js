2636d772e56b0a3c2e2992154a3c3dc96e91505c
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-05-22 23:44:38 -0700

    Create vanishing_gradient_problem.js

---
+++ tasks/vanishing_gradient_problem.js
@@ -0,0 +1,6 @@
+    "what": [
+      "The tendency for the gradients of early hidden layers of some deep neural networks to become surprisingly flat (low). Increasingly lower gradients result in increasingly smaller changes to the weights on nodes in a deep neural network, leading to little or no learning. Models suffering from the vanishing gradient problem become difficult or impossible to train. Long Short-Term Memory cells address this issue.",
+      "Compare to exploding gradient problem."
+      "[related] tendency, gradients, early, hidden layer, become, flat, lower gradients, result, smaller weights, nodes, deep, neural network, no learning, Models, suffering, vanishing gradient problem, difficult, impossible to train, long short-term, Memory cells, address this issue",
+      "[関連している Kanren shite iru] 傾向 Keikō, 勾配 Kōbai, 早い Hayai, 隠しレイヤー, 成る, 平, より低い勾配, 結果, 小さいウェイト, 節点, 深い, 神経網, に導く, 学習なし, モデル, 苦しむ, 勾配消失問題, 難しい, 訓練することは不可能, 長期短期, メモリーセル, この問題に対処する",
+    ],
