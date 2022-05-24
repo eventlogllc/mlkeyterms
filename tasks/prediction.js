b8f259b474c1c4873f8a1c0939db03193925f6c0
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-05-23 21:12:04 -0700

    Create prediction.js

---
+++ tasks/prediction.js
@@ -0,0 +1,6 @@
+    "what": [
+      "A model's output when provided with an input example.",
+      "[英] [A model's output], [provided], [with an input example] ",
+      "[和] [モデルの出力], [提供], [入力例付き] ",
+      "[漢] [模型的輸出], [提供], [帶有輸入示例]"
+    ],
