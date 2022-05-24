aea6b9b9e3431513b4c8b0e5cfe8a234efb03567
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-05-23 21:19:54 -0700

    Create label.js

---
+++ tasks/label.js
@@ -0,0 +1,6 @@
+"what": [
+      "In supervised learning, the \"answer\" or \"result\" portion of an example. Each example in a labeled dataset consists of one or more features and a label. For instance, in a housing dataset, the features might include the number of bedrooms, the number of bathrooms, and the age of the house, while the label might be the house's price. In a spam detection dataset, the features might include the subject line, the sender, and the email message itself, while the label would probably be either \"spam\" or \"not spam.\"",
+       "[英] [In supervised learning] [the answer]  [result] [portion of an example]",
+      "[和] [教師あり学習], [回答], [結果], [例の一部]",	    
+      "[漢] [在監督學習中], [答案], [結果], [部分示例],
+    ],
