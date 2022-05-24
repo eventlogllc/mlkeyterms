71befb084831564249aff95f665766099f508ef4
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-05-23 21:50:39 -0700

    Create Markov_property.js

---
+++ tasks/Markov_property.js
@@ -0,0 +1,6 @@
+"what": [
+      "A property of certain environments, where state transitions are entirely determined by information implicit in the current state and the agent's action.",
+      "[英] [A property of certain environments], [state transitions], [ are entirely determined by information implicit], [in the current state and the agent's action]",
+      "[和] [特定の環境のプロパティ], [状態遷移], [暗黙の情報によって完全に決定されます], [現在の状態とエージェントのアクション]",	    
+      "[漢] [某些環境的屬性], [狀態轉換], [完全由隱含的信息決定], [在當前狀態和代理的動作中]",
+    ],
