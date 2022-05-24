3b3324c524d05c465ef53b73423bb999de737ad7
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-05-23 21:42:22 -0700

    Create Long_Short_Term_Memory__LSTM_.js

---
+++ tasks/Long_Short_Term_Memory__LSTM_.js
@@ -0,0 +1,6 @@
+    "what": [
+      "A type of cell in a recurrent neural network used to process sequences of data in applications such as handwriting recognition, machine translation, and image captioning. LSTMs address the vanishing gradient problem that occurs when training RNNs due to long data sequences by maintaining history in an internal memory state based on new input and context from previous cells in the RNN.".
+      "[英] [A type of cell], [recurrent], [neural network], [RNN], [to process sequences of data], [applications], [handwriting recognition], [machine translation],  [image captioning], [Long Short-Term Memory (LSTM)], [ address vanishing gradient problem], [by maintaining history], [an internal memory state], [based on], [new input], [context from previous cells]",
+      "[和] [セルのタイプ]、[リカレント]、[ニューラルネットワーク]、[RNN]、[データのシーケンスを処理する]、[アプリケーション]、[手書き認識]、[機械翻訳]、[画像キャプション]、[長期 短期記憶（LSTM）]、[勾配消失問題への対処]、[履歴の維持による]、[内部記憶状態]、[に基づく]、[新しい入力]、[前のセルからのコンテキスト]" ,
+      "[漢] [一種細胞], [循環], [神經網絡], [RNN], [處理數據序列], [應用程序], [手寫識別], [機器翻譯], [圖像字幕], [長期 短期記憶（LSTM）], [解決梯度消失問題], [通過維護歷史], [內部記憶狀態]，[基於], [新輸入], [來自先前單元格的上下文]",
+    ],
