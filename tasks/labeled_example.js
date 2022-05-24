82c99146ff13086583f48c296130456543d76308
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-05-23 21:26:30 -0700

    Create labeled_example.js

---
+++ tasks/labeled_example.js
@@ -0,0 +1,6 @@
+"what": [
+      "An example that contains features and a label. In supervised training, models learn from labeled examples.",
+      "[英] [An example], [contains], [features and a label]",
+      "[和] [示例], [包含], [特徵和標籤]",	    
+      "[漢] [例], [含む], [機能とラベル]",
+    ],
