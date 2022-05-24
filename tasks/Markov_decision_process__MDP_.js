5d53e685514a046761c93b71d8be9328854fe284
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-05-23 20:58:16 -0700

    Update Markov_decision_process__MDP_.js

--- tasks/Markov_decision_process__MDP_.js
+++ tasks/Markov_decision_process__MDP_.js
@@ -1,3 +1,7 @@
  "what": [
-      "A graph representing the decision-making model where decisions (or actions) are taken to navigate a sequence of states under the assumption that the Markov property holds. In reinforcement learning, these transitions between states return a numerical reward."
+      "A graph representing the decision-making model where decisions (or actions) are taken to navigate a sequence of states under the assumption that the Markov property holds. In reinforcement learning, these transitions between states return a numerical reward.",
+      "[英] [A graph] [representing] [the decision-making model]  [decisions (or actions)]  [navigate ] [a sequence of states] [under the assumption] [Markov property holds]",
+      "[和] [グラフ][表現][意思決定モデル][意思決定（またはアクション）][ナビゲート][一連の状態][仮定の下で][マルコフ性が成り立つ]",
+      "[漢] [圖] [表示] [決策模型] [決策（或行動）] [導航] [狀態序列] [在假設下] [馬爾可夫屬性成立]",
+  
     ],
