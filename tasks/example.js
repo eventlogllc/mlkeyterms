876ab5ce3d61801dd573e4cc08418202f39fe5b4
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-05-23 21:23:33 -0700

    Create example.js

---
+++ tasks/example.js
@@ -0,0 +1,6 @@
+"what": [
+      "One row of a dataset. An example contains one or more features and possibly a label.",
+      "[英] [One row of a dataset], [An example], [contains], [one or more features], [possibly a label]",
+      "[和] [データセットの1行], [例], [含む], [1つ以上の機能], [おそらくラベル]",	    
+      "[漢] [數據集的一行], [一個示例], [包含], [一個或多個特徵], [可能是一個標籤],
+    ],
