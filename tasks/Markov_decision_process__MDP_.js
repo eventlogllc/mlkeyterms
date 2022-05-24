4a7bb30b1bffc5a6e311b277c7a60a3abcf1553a
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-05-23 20:53:31 -0700

    Create Markov_decision_process__MDP_.js

---
+++ tasks/Markov_decision_process__MDP_.js
@@ -0,0 +1,3 @@
+ "what": [
+      "A graph representing the decision-making model where decisions (or actions) are taken to navigate a sequence of states under the assumption that the Markov property holds. In reinforcement learning, these transitions between states return a numerical reward."
+    ],
