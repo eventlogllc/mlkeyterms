885c8daffac2d22899febc07b48895e123a4c4fb
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-05-23 21:15:53 -0700

    Create feature.js

---
+++ tasks/feature.js
@@ -0,0 +1,6 @@
+"what": [
+      "An input variable used in making predictions.",
+      "[英] input variable, making, predictions, ",
+      "[和] [入力変数], [予測を行う]",
+      "[漢] [輸入變量], [做出預測]",
+    ],
