909fa874f67c202c7ba722f4e7a74440fb60c41d
akiko-113 <104332806+akiko-113@users.noreply.github.com>
2022-05-29 23:27:21 +0900

    Update Markov_property.js

--- tasks/Markov_property.js
+++ tasks/Markov_property.js
@@ -4,3 +4,9 @@
       "[和] [特定の環境のプロパティ], [状態遷移], [暗黙の情報によって完全に決定されます], [現在の状態とエージェントのアクション]",	    
       "[漢] [某些環境的屬性], [狀態轉換], [完全由隱含的信息決定], [在當前狀態和代理的動作中]",
     ],
+
+      [A property of certain environments],  [特定の環境のプロパティ][tokutei no kankyo no puropaty][とくていのかんきょうのプロパティ],[某些環境的屬性]
+      [state transitions], [状態遷移][joutai senni][じょうたいせんい], [狀態轉換]
+      [ are entirely determined by information implicit], [暗黙の情報によって完全に決定されます][annmoku no jouhou ni yotte kannzenn ni kettei saremasu][あんもくのじょうほうによってかんぜんにけっていされます], [完全由隱含的信息決定]
+      [in the current state and the agent's action]",[現在の状態とエージェントのアクション][genzai no jouai to eijento no akushion][げんざいのじょうたいとエージェントのアクション], [在當前狀態和代理的動作中]
+      
