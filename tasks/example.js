f7421fdff1dd324028912f29eaf8a93c039a5372
akiko-113 <104332806+akiko-113@users.noreply.github.com>
2022-05-29 23:47:01 +0900

    Update example.js

--- tasks/example.js
+++ tasks/example.js
@@ -4,3 +4,10 @@
       "[和] [データセットの1行], [例], [含む], [1つ以上の機能], [おそらくラベル]",	    
       "[漢] [數據集的一行], [一個示例], [包含], [一個或多個特徵], [可能是一個標籤],
     ],
+
+      [One row of a dataset], [データセットの1行][deita setto no ichi gyou][データセットのいちぎょう][數據集的一行]
+      [An example], [例], [rei][れい],[一個示例]
+      [contains],  [含む][fukumu][ふくむ], [包含]
+      [one or more features],[1つ以上の機能][hitotsu ijou no kinou][ひとついじょうのきのう],[一個或多個特徵]
+      [possibly a label]",[おそらくラベル],[osoraku raberu][おそらくラベル] [可能是一個標籤],
+    
