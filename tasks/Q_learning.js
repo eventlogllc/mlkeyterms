2263679452847731badc55e85269a5e678493ef2
akiko-113 <104332806+akiko-113@users.noreply.github.com>
2022-05-29 23:32:50 +0900

    Update Q_learning.js

--- tasks/Q_learning.js
+++ tasks/Q_learning.js
@@ -6,3 +6,11 @@
       "[和] [強化学習][アルゴリズム], [許可], [エージェント], [最適なQ関数を学習する], [適用], [マルコフ決定過程], [ベルマン方程式]",
       "[漢] [在強化學習中], [一個算法], [允許], [一個代理], [學習最優 Q 函數], [應用], [馬爾可夫決策過程], [貝爾曼方程] ",
 ],
+"[英] [In reinforcement learning], [強化学習][kyouka gakushu][きょうかがくしゅう],[在強化學習中]
+[an algorithm], [アルゴリズム][arugorizumu][アルゴリズム], [一個算法]
+      [allow], [許可する][kyokasuru][きょかする],[允許]
+      [an agent],  [エージェント][eejento][エージェント],[一個代理]
+      [to learn the optimal Q-function], [最適なQ関数を学習する][saitekina Qkansuu wo gakusyu suru][さいてきなＱかんすうをがくしゅうする], [學習最優 Q 函數]
+      [applying], [適用][tekiyou][てきよう], [應用]
+      [Markov decision process],[マルコフ決定過程][marukofu kettei katei][マルコフけっていかてい], [馬爾可夫決策過程]
+      [ Bellman equation]", [ベルマン方程式][beruman houteishiki][ベルマンほうていしき], [貝爾曼方程] 
