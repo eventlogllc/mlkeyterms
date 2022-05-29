da1441d2f21fed3df3b5b6166dba0a12ea39973c
akiko-113 <104332806+akiko-113@users.noreply.github.com>
2022-05-30 00:15:00 +0900

    Update vanishing_gradient_problem.js

--- tasks/vanishing_gradient_problem.js
+++ tasks/vanishing_gradient_problem.js
@@ -5,3 +5,26 @@
       "[和] [関連] 傾向, 勾配 , 早い, 隠しレイヤー, 成る, 平, より低い勾配, 結果, 小さいウェイト, 節点, 深い, 神経網, に導く, 学習なし, モデル, 苦しむ, 勾配消失問題, 難しい, 訓練することは不可能, 長期短期, メモリーセル, この問題に対処する",
       "[漢] 趨勢，梯度，早期，隱藏層，變得，平坦，梯度較低，結果，較小的權重，節點，深度，神經網絡，無學習，模型，痛苦，梯度消失問題，困難，不可能訓練，長期短期， 記憶細胞，解決這個問題",
     ],
+[related]  [関連][kanren][かんれん] 
+[tendency]傾向[keikou][けいこう],趨勢
+        [gradients]勾配[koubai][こうばい],，梯度
+        [early] 早い[hayai][はやい] ，早期，
+        [hidden layer]隠しレイヤー[kakushi reiya-][かくしレイヤー], 隱藏層
+        [become]成る[naru][なる], ，變得
+[flat],  平,[taira][たいら] ，平坦
+        [lower gradients]より低い勾配,[yori hikui koubai][よりひくいこうばい]，梯度較低
+        [result],  結果[kekka][けっか],結果
+        [smaller weights],  小さいウェイト,[chiisai weito][ちいさいウェイト]，較小的權重
+        [nodes] 節点[setten][せってん], 節點
+        [deep], 深い[fukai][ふかい], 深度
+        [neural network]ニューラルネットワーク[nyuraru netto waaku][ニューラルネットワーク], 神經網絡
+        [no learning] 学習なし[gakusyuu nashi][がくしゅうなし],無學習
+        [Models] モデル[moderu][モデル], 模型
+        [suffering]苦しむ[kurushimu][くるしむ], 痛苦
+        [vanishing gradient problem]勾配消失問題[koubai shoushitsu mondai][こうばいしょうしつもんだい]，梯度消失問題
+        [difficult]難しい[muzukashii][むずかしい], 困難
+        [impossible to train]訓練することは不可能[kunnrennsurukoto wa fukanou][くんれんすることはふかのう]，不可能訓練
+        [long short-term], 長・短期[chou・tanki][ちょう・たんき]，長期短期
+        [Memory cells],  メモリーセル[memori seru][メモリーセル]， 記憶細胞
+        [address this issue], この問題に対処する[kono mondai ni taisho suru][このもんだいにたいしょする]，解決這個問題
+
