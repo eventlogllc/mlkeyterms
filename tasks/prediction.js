2322301149885d51ffeed884e3a6901edc675726
akiko-113 <104332806+akiko-113@users.noreply.github.com>
2022-05-29 23:59:45 +0900

    Update prediction.js

--- tasks/prediction.js
+++ tasks/prediction.js
@@ -4,3 +4,7 @@
       "[和] [モデルの出力], [提供], [入力例付き] ",
       "[漢] [模型的輸出], [提供], [帶有輸入示例]"
     ],
+
+        [A model's output], [モデルの出力] [moderu no shutsuryoku][モデルのしゅつりょく],[模型的輸出]
+         [provided], [提供][teikyou][ていきょう], [提供]
+        [with an input example] , [入力例付き][nyuuryoku rei tsuki][にゅうりょくれいつき] , [帶有輸入示例]
