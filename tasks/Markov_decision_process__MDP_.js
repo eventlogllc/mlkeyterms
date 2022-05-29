1d7b2e7365fdf0fbcfd8e19d05c84dbd84b04c80
akiko-113 <104332806+akiko-113@users.noreply.github.com>
2022-05-29 23:21:28 +0900

    Update Markov_decision_process__MDP_.js

--- tasks/Markov_decision_process__MDP_.js
+++ tasks/Markov_decision_process__MDP_.js
@@ -5,3 +5,12 @@
       "[漢] [圖], [表示], [決策模型], [決策（或行動）], [導航], [狀態序列], [在假設下], [馬爾可夫屬性成立]",
   
     ],
+ [A graph],[グラフ][gurafu][グラフ],[圖]
+ [representing] ,[表現する][hyougen suru][ひょうげんする], [表示]
+ [the decision-making model] ,[意思決定モデル][ishi kettei moderu][いしけっていモデル], [決策模型]
+ [decisions (or actions)] ,[意思決定（またはアクション）][ishi kettei matawa akushion][いしけってい（またはアクション）], [決策（或行動）]
+ [navigate ] ,[ナビゲート][nabigeito][ナビゲート], [導航]
+ [a sequence of states] ,[一連の状態], [ichiren no joutai][いちれんのじょうたい][狀態序列]
+ [under the assumption] ,[仮定の下で][katei no moto de][かていのもとで], [在假設下]
+ [Markov property holds]",[マルコフ性が成り立つ],[marukofu sei ga naritatsu][マルコフせいがなりたつ] [馬爾可夫屬性成立]
+    
