9b4bd664f5d072ff71da21a70d8b60bf1d17d72b
akiko-113 <104332806+akiko-113@users.noreply.github.com>
2022-05-29 23:50:30 +0900

    Update feature.js

--- tasks/feature.js
+++ tasks/feature.js
@@ -4,3 +4,5 @@
       "[和] [入力変数], [予測を行う]",
       "[漢] [輸入變量], [做出預測]",
     ],
+[input variable],  [入力変数][nyuuryoku hensuu][にゅうりょくへんすう],[輸入變量]
+[making predictions],  [予測を行う][yosoku wo okonau][よそくをおこなう], [做出預測]
