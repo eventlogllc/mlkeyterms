064c4c46e595cd88b4434901e9c63b083c8b8932
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-06-09 08:32:00 -0700

    Update Bellman_equation.js

--- tasks/tmp/Bellman_equation.js
+++ tasks/tmp/Bellman_equation.js
@@ -3,6 +3,9 @@
       "The Bellman Equation follows a specific policy and function to give you the best value based on the states, ",
       "which it calculated recursively through dividing the states/paths up into subproblems or smaller problems. ",
       "For example, it might do 'State 0 to State 1 or State 2' and find out the optimal value and then it will find 'State 2 to State 3' and find optimal value.",
-      "And then give you the most optimal value."
+      "And then give you the most optimal value.",
+      "[英] [related] The Bellman Equation, follows a specific policy and function, to give you, the best value, based on the states, it calculated recursively through dividing the states/paths up into subproblems or smaller problems. ",
+      "[和] [関連] [ベルマン方程式], [特定のポリシーと機能に従う], [与える], [最良の値], [状態に基づく], [再帰的に計算する], [状態/パスをサブ問題に分割することにより または小さな問題]",	    
+      "[漢] [相關] [貝爾曼方程], [遵循特定的策略和功能], [給你], [最佳價值], [基於狀態], [遞歸計算], [通過將狀態/路徑劃分為子問題 或更小的問題]",
     ],
 
