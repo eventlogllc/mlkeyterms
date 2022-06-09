d63d23e8078e86f6b1d58153f171ae7b7fadcd2c
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-06-09 08:22:45 -0700

    Create Bellman_equation.js

---
+++ tasks/tmp/Bellman_equation.js
@@ -0,0 +1,8 @@
+
+"what": [
+      "The Bellman Equation follows a specific policy and function to give you the best value based on the states, ",
+      "which it calculated recursively through dividing the states/paths up into subproblems or smaller problems. ",
+      "For example, it might do 'State 0 to State 1 or State 2' and find out the optimal value and then it will find 'State 2 to State 3' and find optimal value.",
+      "And then give you the most optimal value."
+    ],
+
