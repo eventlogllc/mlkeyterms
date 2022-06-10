28a443e7259c86e71eb36a52ccbaefd09af36fa7
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-06-10 08:04:28 -0700

    Update Bellman_equation.js

--- tasks/tmp/Bellman_equation.js
+++ tasks/tmp/Bellman_equation.js
@@ -6,6 +6,6 @@
       "And then give you the most optimal value.",
       "[英] [related] [A function], [(for example, ReLU or sigmoid)], [takes in the weighted sum of all of the inputs], [from the previous layer], [and then generates and passes an output value], [(typically nonlinear)], [to the next layer.]",
       "[和] [関数], [（たとえば、ReLUまたはシグモイド）], [すべての入力の加重和を取ります], [前のレイヤーから], [そして出力値を生成して渡します], [（通常は非線形）], [次のレイヤーへ]",	    
-      "[漢] [一個函數], [（例如，ReLU 或 sigmoid）], [獲取所有輸入的加權和], [來自上一層], [然後生成並傳遞一個輸出值], [（通常是非線性的）], [到下一層]",
+      "[漢] [相關], [一個函數], [（例如，ReLU 或 sigmoid）], [獲取所有輸入的加權和], [來自上一層], [然後生成並傳遞一個輸出值], [（通常是非線性的）], [到下一層]",
     ],
 
