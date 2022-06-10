ddf50b3d9d57b95e5c51fbe4341f9e7d7db4e437
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-06-10 08:23:22 -0700

    Create squared_loss.js

---
+++ tasks/tmp/squared_loss.js
@@ -0,0 +1,6 @@
+"what": [
+      "The loss function used in linear regression.  (Also known as L2 Loss.) This function calculates the squares of the difference between a model's predicted value for a labeled example and the actual value of the label. Due to squaring, this loss function amplifies the influence of bad predictions. That is, squared loss reacts more strongly to outliers than L1 loss.",
+      "[英] [related] [The loss function used in linear regression] [Also known as L2 Loss] [This function calculates] [the squares of the difference between a model's predicted value for a labeled example and the actual value of the label] [Due to squaring] [this loss function ] [amplifies the influence of bad predictions] [That is] [squared loss reacts more strongly to outliers than L1 loss]",
+      "[和] [関数] [線形回帰で使用される損失関数][L2損失とも呼ばれます][この関数は計算します][ラベル付けされた例のモデルの予測値とラベルの実際の値の差の2乗][二乗による][ この損失関数][悪い予測の影響を増幅します][つまり][二乗損失はL1損失よりも外れ値に強く反応します]",	    
+      "[漢] [相關] [線性回歸中使用的損失函數] [也稱為L2損失] [此函數計算] [模型對標記示例的預測值與標籤實際值之間的差的平方] [由於平方] [ 這個損失函數] [放大了錯誤預測的影響] [即] [平方損失對異常值的反應比 L1 損失更強烈]",
+    ],
