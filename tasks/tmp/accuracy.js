b3e7bf8190845f05981314328efa46ea7ff16347
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-06-10 08:15:55 -0700

    Update accuracy.js

--- tasks/tmp/accuracy.js
+++ tasks/tmp/accuracy.js
@@ -1,4 +1,7 @@
     "what": [
       "The fraction of predictions that a classification model got right. In multi-class classification, accuracy is defined as follows:",
-      "分類 モデル が 正しい た 予測 の 割合 。  マルチ クラス 分類 で は 、 精度 は 次 の よう に 定義 する れる ます 。",
+       "\\text{Accuracy} =\n\\frac{\\text{Correct Predictions}} {\\text{Total Number Of Examples}}",
+      "[英] [The fraction of predictions], [a classification model got right].",
+      "[和] [[予測の割合]、[分類モデルが正しかった]",	    
+      "[漢] [預測的分數]，[分類模型正確]",
     ],
