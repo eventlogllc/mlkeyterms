1aba5cf443bd1f1046a86e34d7b9d2cd51027b85
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-06-10 08:09:32 -0700

    Create accuracy.js

---
+++ tasks/tmp/accuracy.js
@@ -0,0 +1,4 @@
+    "what": [
+      "The fraction of predictions that a classification model got right. In multi-class classification, accuracy is defined as follows:",
+      "分類 モデル が 正しい た 予測 の 割合 。  マルチ クラス 分類 で は 、 精度 は 次 の よう に 定義 する れる ます 。",
+    ],
