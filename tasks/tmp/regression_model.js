2c1faa0dfad7006086208b985378578a0e511137
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-06-11 10:22:14 -0700

    Create regression_model.js

---
+++ tasks/tmp/regression_model.js
@@ -0,0 +1,6 @@
+"what": [
+      "A type of model that outputs continuous (typically, floating-point) values. Compare with classification models, which output discrete values, such as 'day lily' or 'tiger lily'."
+      "[英] [related] [A type of model] [outputs continuous (typically, floating-point) values]. [Compare with classification models] [output discrete values] [such as 'day lily' or 'tiger lily']",
+      "[和] [関連] [モデルの種類][連続（通常は浮動小数点）値を出力][分類モデルと比較][離散値を出力][「カンゾウ」や「トラユリ」など]",	    
+      "[漢] [相關] [一種模型] [輸出連續（通常是浮點）值] [與分類模型比較] [輸出離散值] [比如‘day lily’或者‘tiger lily’]",
+],
