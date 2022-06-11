a27077442a882f3af9ba5e4275898b7113a7c02e
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-06-11 10:38:07 -0700

    Create linear_regression.js

---
+++ tasks/tmp/linear_regression.js
@@ -0,0 +1,9 @@
+"what": [
+      "Using the raw output (y') of a linear model as the actual prediction in a regression model.",
+      "The goal of a regression problem is to make a real-valued prediction.",
+      "For example, if the raw output (y') of a linear model is 8.37, then the prediction is 8.37.",
+      "Contrast linear regression with logistic regression. Also, contrast regression with classification.",
+      "[英] [related] [Using the raw output (y') of a linear model] [as the actual prediction] [in a regression model] [The goal of a regression problem is to make a real-valued prediction] [For example] [if the raw output (y') of a linear model is 8.37] [then the prediction is 8.37]",
+      "[和] [関連] [線形モデルの生の出力（y'）を使用][実際の予測として] [回帰モデルで] [回帰問題の目的は、実数値の予測を行うことです] [例] [線形モデルの生の出力（y'）が8.37の場合] [予測は8.37です]",	    
+      "[漢] [相關] [使用線性模型的原始輸出 (y')] [作為實際預測] [在回歸模型中] [回歸問題的目標是進行實值預測] [例如] [如果線性模型的原始輸出 (y') 是 8.37] [那麼預測是 8.37]",
+    ],
