0352eba262e9fc162a41422e94fc4848c1108b6e
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-06-11 10:43:25 -0700

    Create classification_model.js

---
+++ tasks/tmp/classification_model.js
@@ -0,0 +1,6 @@
+    "what": [
+      "A type of machine learning model for distinguishing among two or more discrete classes. For example, a natural language processing classification model could determine whether an input sentence was in French, Spanish, or Italian. Compare with regression model.",
+      "[英] [related] [A type of machine learning model] [for distinguishing among two or more discrete classes] [For example] [a natural language processing] [classification model] [could determine whether an input sentence] [was in French, Spanish, or Italian] [Compare with regression model]",
+      "[和] [関連] [機械学習モデルの一種] [2つ以上の個別のクラスを区別するため] [例] [自然言語処理] [分類モデル][入力文がフランス語, スペイン語, イタリア語のいずれであるかを判断できた] [回帰モデルと比較]",	    
+      "[漢] [相關] [貝爾曼方程] [遵循特定的策略和功能] [給你] [最佳價值] [基於狀態] [遞歸計算] [通過將狀態/路徑劃分為子問題 或更小的問題]",
+    ],
