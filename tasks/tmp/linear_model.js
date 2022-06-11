852fa7b8ae0e89bd4232242e00bd5b3e565a4719
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-06-11 10:30:38 -0700

    Create linear_model.js

---
+++ tasks/tmp/linear_model.js
@@ -0,0 +1,8 @@
+"what": [
+      "A model that assigns one weight per feature to make predictions. (Linear models also incorporate a bias.) By contrast, the relationship of weights to features in  deep models is not one-to-one.",
+      "A linear model uses the following formula:",
+      "y' = b + w_1 * x_1 + w_2 * x_2 + ... + w_n * x_n ",
+      "[英] [related] [A model] [ that assigns one weight per feature] [to make predictions] [Linear models also incorporate a bias]",
+      "[和] [関連] [モデル] [特徴ごとに1つの重みを割り当てる] [予測を行うため] [線形モデルにもバイアスが組み込まれています]",	    
+      "[漢] [相關] [一個模型] [為每個特徵分配一個權重] [進行預測] [線性模型也包含偏差]",
+    ],
