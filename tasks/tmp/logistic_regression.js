7c1124a52cd555323139c5617e75c0e93ad1e3ff
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-06-11 10:12:49 -0700

    Create logistic_regression.js

---
+++ tasks/tmp/logistic_regression.js
@@ -0,0 +1,11 @@
+"what": [
+      "A classification model that uses a sigmoid function to convert a linear model's raw prediction (y') into a value between 0 and 1. You can interpret the value between 0 and 1 in either of the following two ways:",
+      "As a probability that the example belongs to the positive class in a binary classification problem.",
+      "As a value to be compared against a classification threshold. If the value is equal to or above the classification threshold, the system classifies the example as the positive class. Conversely, if the value is below the given threshold, the system classifies the example as the negative class",
+      "For example, suppose the classification threshold is 0.82:",
+      "Imagine an example that produces a raw prediction (y') of 2.6. The sigmoid of 2.6 is 0.93. Since 0.93 is greater than 0.82, the system classifies this example as the positive class.",
+      "Imagine a different example that produces a raw prediction of 1.3. The sigmoid of 1.3 is 0.79. Since 0.79 is less than 0.82, the system classifies that example as the negative class.",
+      "[英] [related] [A classification model] [that uses a sigmoid function to convert a linear model's raw prediction (y')] [into a value between 0 and 1]. [You can interpret the value between 0 and 1] [in either of the following two ways:] [As a probability] [that the example belongs to the positive class] [in a binary classification problem] [As a value to be compared against a classification threshold] [If the value is equal to or above the classification threshold] [the system classifies the example as the positive class] [Conversely] [if the value is below the given threshold] [the system classifies the example as the negative class]",
+      "[和] [関連] [分類モデル][シグモイド関数を使用して線形モデルの生の予測（y'）を変換する][0から1の間の値に変換する][0から1の間の値を解釈できます][次の2つのいずれかで 方法：] [- 確率として][例が正のクラスに属すること][バイナリ分類問題で] [- 分類しきい値と比較する値として][値が分類しきい値以上の場合][システムは例を正のクラスとして分類します][逆に][値が指定されたしきい値を下回っている場合] [システムは例をネガティブクラスとして分類します]",	    
+      "[漢] [相關] [一個分類模型] [使用sigmoid函數將線性模型的原始預測（y'）] [轉換為0到1之間的值] [可以解釋0到1之間的值] [以下兩種 方法：] [- 作為概率] [示例屬於正類] [在二元分類問題中] [- 作為要與分類閾值進行比較的值] [如果該值等於或高於分類閾值] [系統將示例分類為正類] [相反] [如果該值低於給定閾值] [ 系統將示例分類為負類]",
+  ]
