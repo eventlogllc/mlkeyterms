4db335134d201105a6c5238b247a2570d3c4de12
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-06-11 09:46:23 -0700

    Create loss.js

---
+++ tasks/tmp/loss.js
@@ -0,0 +1,6 @@
+"what": [
+      "A measure of how far a model's predictions are from its label. Or, to phrase it more pessimistically, a measure of how bad the model is. To determine this value, a model must define a loss function. For example, linear regression models typically use mean squared error for a loss function, while logistic regression models use Log Loss.",
+      "[英] [related] [A measure of how far a model's predictions are from its label] [Or, to phrase it more pessimistically] [a measure of how bad the model is] [To determine this value, a model must define a loss function] [For example] [linear regression models] [typically use mean squared error] [for a loss function] [while logistic regression models] [use Log Loss]",
+      "[和] [関連] [モデルの予測がそのラベルからどれだけ離れているかの尺度][または、より悲観的に表現するには][モデルの悪さの尺度][この値を決定するには、モデルは損失関数を定義する必要があります] [たとえば、 ][線形回帰モデル][通常は平均二乗誤差を使用][損失関数の場合][ロジスティック回帰モデルの場合][対数損失を使用]",	    
+      "[漢] [相關] [[衡量模型的預測與其標籤的距離] [或者，更悲觀地說] [衡量模型有多糟糕] [要確定這個值，模型必須定義損失函數] [例如 ] [線性回歸模型] [通常使用均方誤差] [對於損失函數] [而邏輯回歸模型] [使用對數損失]",
+    ],
