8ae5031d15e26234a761ad098c2d908b8a2b6e02
akiko-113 <104332806+akiko-113@users.noreply.github.com>
2022-06-14 18:07:15 +0900

    Update loss.js

--- tasks/tmp/loss.js
+++ tasks/tmp/loss.js
@@ -4,3 +4,18 @@
       "[和] [関連] [モデルの予測がそのラベルからどれだけ離れているかの尺度][または、より悲観的に表現するには][モデルの悪さの尺度][この値を決定するには、モデルは損失関数を定義する必要があります] [たとえば、 ][線形回帰モデル][通常は平均二乗誤差を使用][損失関数の場合][ロジスティック回帰モデルの場合][対数損失を使用]",	    
       "[漢] [相關] [[衡量模型的預測與其標籤的距離] [或者，更悲觀地說] [衡量模型有多糟糕] [要確定這個值，模型必須定義損失函數] [例如 ] [線性回歸模型] [通常使用均方誤差] [對於損失函數] [而邏輯回歸模型] [使用對數損失]",
     ],
+
+      [英] [related] "[和] [関連][kanren][かんれん],[漢] [相關]
+[A measure of how far a model's predictions are from its label] [モデルの予測がそのラベルからどれだけ離れているかの尺度]
+ [moderu no yosoku ga sono raberu kara doredake hanareteirunoka no shakudo][モデルのよそくがそのラベルからどれだけはなれているのかのしゃくど]
+ [[衡量模型的預測與其標籤的距離]
+ [Or, to phrase it more pessimistically]  [または、より悲観的に表現するには][matawa yori hikantekini hyougen suruniwa][または、よりひかんてきにひょうげんするには] [或者，更悲觀地說] 
+[a measure of how bad the model is] [モデルの悪さの尺度は][moderu no warusa no shakudo wa][モデルのわるさのしゃくどは][衡量模型有多糟糕]
+[To determine this value, a model must define a loss function]  [この値を決定するには、モデルは損失関数を定義する必要がある]
+[kono atai wo kettei suruniwa, moderu ha sonshitsu kannsuu wo teigisuru hitsuyougaaru][このあたいをけっていするには、モデルはそんしつかんすうをていぎするひつようがある][要確定這個值，模型必須定義損失函數] 
+[For example][たとえば、 ][tatoeba][たとえば][例如 ]
+[linear regression models]  [線形回帰モデル][senkei kaiki moderu][せんけいかいきモデル][線性回歸模型]
+[typically use mean squared error]  [通常は平均二乗誤差を使用][tsuujou wa heikin nijou gosa wo shiyou][つうじょうはへいきんにじょうごさをしよう][通常使用均方誤差] 
+[for a loss function] [損失関数の場合][sonshitsu kannsuu no baai][そんしつかんすうのばあい][對於損失函數] 
+[while logistic regression models][ロジスティック回帰モデルの場合] [rojisutic kaiki moderu no baai][ロジスティクかいきモデルのばあい][而邏輯回歸模型]
+[use Log Loss], [対数損失を使用][taisuu sonshitsu wo shiyou][たいすうそんしつをしよう] [使用對數損失]
