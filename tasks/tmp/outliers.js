73df10d65ebae9c599bbdb398253c862a25b3834
akiko-113 <104332806+akiko-113@users.noreply.github.com>
2022-06-14 18:24:14 +0900

    Update outliers.js

--- tasks/tmp/outliers.js
+++ tasks/tmp/outliers.js
@@ -8,3 +8,17 @@
       "[和] [関連] [他のほとんどの値から離れた値][機械学習の場合][次のいずれかが異常値です][絶対値が高い][実際の値から比較的離れた予測値][値が約3標準偏差を超える入力データ 平均から]",	    
       "[漢] [相關] [與大多數其他值相距甚遠的值] [在機器學習中] [以下任何一項都是異常值] [具有高絕對值] [預測值與實際值相距較遠] [輸入數據的值大於大約 3 個標準差 從平均數]",
     ],
+
+        "[英] [related] "[和] [関連]"[漢] [相關]
+    [Values distant from most other values] [他のほとんどの値から離れた値][與大多數其他值相距甚遠的值]
+    [In machine learning] [機械学習の場合][在機器學習中] 
+    [any of the following are outliers][次のいずれかが異常値です] [以下任何一項都是異常值] 
+    [with high absolute values] [絶対値が高い][具有高絕對值]
+    [Predicted values relatively far away from the actual values] [実際の値から比較的離れた予測値] [預測值與實際值相距較遠]
+    [Input data whose values are more than roughly 3 standard deviations from the mean],[ 平均から値が約3標準偏差を超える入力データ]
+        [heikin kara atai ga yaku 3 hyoujunn hennsa wo koeru nyuuryoku deta][へいきんからあたいがやく３ひょうじゅんへんさをこえるにゅうりょくデータ][輸入數據的值大於大約 3 個標準差 從平均數]",
+    [Outliers often cause problems in model training][異常値はモデルトレーニングでよく問題を起こす][ijouchi wa moderu treiningu de yoku mondai wo okosu]
+[Clipping is one way of managing outliers][クリッピングは異常値を管理するひとつの方法だ][kurippingu wa ijouchi wo kanrisuru hitotsu no houhou da]
+
+        
+    ],
