b7455bd7ba39aca6f6a4983fbfe7dde50baba826
akiko-113 <104332806+akiko-113@users.noreply.github.com>
2022-06-14 18:42:50 +0900

    Update squared_loss.js

--- tasks/tmp/squared_loss.js
+++ tasks/tmp/squared_loss.js
@@ -4,3 +4,18 @@
       "[和] [関数] [線形回帰で使用される損失関数][L2損失とも呼ばれます][この関数は計算します][ラベル付けされた例のモデルの予測値とラベルの実際の値の差の2乗][二乗による][ この損失関数][悪い予測の影響を増幅します][つまり][二乗損失はL1損失よりも外れ値に強く反応します]",	    
       "[漢] [相關] [線性回歸中使用的損失函數] [也稱為L2損失] [此函數計算] [模型對標記示例的預測值與標籤實際值之間的差的平方] [由於平方] [ 這個損失函數] [放大了錯誤預測的影響] [即] [平方損失對異常值的反應比 L1 損失更強烈]",
     ],
+
+      "[英] [related]"[和] [関数][漢] [相關]
+[The loss function used in linear regression] [線形回帰で使用される損失関数][senkei kaiki de shiyousareru sonshitsu kansuu][せんけいかいきでそようされるそんしつかんすう] [線性回歸中使用的損失函數]
+[Also known as L2 Loss] [L2損失とも呼ばれる][L2sonshitsu tomo yobareru][L2 そんしつともよばれる][也稱為L2損失]
+[This function calculates][この関数は計算する][kono kansuu ha keisan suru][このかんすうはけいさんする] [此函數計算]
+[the squares of the difference between a model's predicted value for a labeled example and the actual value of the label] 
+ [ラベル付けされた例のモデルの予測値とラベルの実際の値の差の2乗]
+[raberuduke sareta reino moderu no yosokuchi to raberu no jissai no atai no sa no nijou][ラベルづけされたれいのモデルとよそくちとラベルのじっさいのあたいのさのにじょう]
+[模型對標記示例的預測值與標籤實際值之間的差的平方]
+ [Due to squaring][二乗による][nijou niyoru][にじょうによる][由於平方]
+[this loss function ] [ この損失関数][kono sonshitsu kannsuu][このそんしつかんすう][ 這個損失函數]
+[amplifies the influence of bad predictions] [悪い予測の影響を増幅する][warui yosoku no eikyou wo zouhukusuru][わるいよそくのえいきょうをぞうふくする][放大了錯誤預測的影響]
+[That is]  [つまり] [tsumari][つまり] [即] 
+[squared loss reacts more strongly to outliers than L1 loss]"[二乗損失はL1損失よりも外れ値に強く反応する][nijou sonshitsu ha L1sonshitsu yorimo hazurechi ni tsuyoku hannnou suru]
+[にじょうそんしつはL1そんしつよりもはずれちにつよくはんのうする][平方損失對異常值的反應比 L1 損失更強烈]"
