69aaba2dc800c89bdfa15121402161ea01d0185a
akiko-113 <104332806+akiko-113@users.noreply.github.com>
2022-06-14 17:32:40 +0900

    Update linear_regression.js

--- tasks/tmp/linear_regression.js
+++ tasks/tmp/linear_regression.js
@@ -7,3 +7,13 @@
       "[和] [関連] [線形モデルの生の出力（y'）を使用][実際の予測として] [回帰モデルで] [回帰問題の目的は、実数値の予測を行うことです] [例] [線形モデルの生の出力（y'）が8.37の場合] [予測は8.37です]",	    
       "[漢] [相關] [使用線性模型的原始輸出 (y')] [作為實際預測] [在回歸模型中] [回歸問題的目標是進行實值預測] [例如] [如果線性模型的原始輸出 (y') 是 8.37] [那麼預測是 8.37]",
     ],
+
+      "[英] [related] "[和] [関連] [kanren][かんれん] "[漢] [相關] 
+[Using the raw output (y') of a linear model] [線形モデルの生の(RAW)出力（y'）を使用][senkei moderu no namano (RAW) autoputto (y') wo shiyou][せんけいモデルのなまの（RAW）アウトプット(y')をしよう] [使用線性模型的原始輸出 (y')] 
+ [as the actual prediction]  [実際の予測として] [jissai no yosoku toshite][じっさいのよそくとして][作為實際預測]
+[in a regression model] [回帰モデルで][kaiki moderu de][かいきモデルで][在回歸模型中] 
+[The goal of a regression problem is to make a real-valued prediction] [回帰問題の目的は、実数値の予測を行うことである][kaiki mondai no mokuteki wa jissuuchi no yosoku wo okonaukoto dearu][かいきもんだいのもくてきは、じっすうちのよそくをおこなうことである]  [回歸問題的目標是進行實值預測] 
+[For example]  [例] [rei][れい][例如] 
+[if the raw output (y') of a linear model is 8.37] [線形モデルの生の出力（y'）が8.37の場合][senkei moderu no namano (RAW) autoputto (y') ga 8.37 no baai][せんけいモデルのなまの（RAW）アウトプット(y')が8.37のばあい][如果線性模型的原始輸出 (y') 是 8.37] 
+ [then the prediction is 8.37]", [予測は8.37である][yosoku wa 8.37 dearu][よそくは8.37である][那麼預測是 8.37]
+  
