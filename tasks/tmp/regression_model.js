d81484353cc6e79bf9b9cf515016c929147fc59f
akiko-113 <104332806+akiko-113@users.noreply.github.com>
2022-06-14 18:57:00 +0900

    Update regression_model.js

--- tasks/tmp/regression_model.js
+++ tasks/tmp/regression_model.js
@@ -4,3 +4,12 @@
       "[和] [関連] [モデルの種類][連続（通常は浮動小数点）値を出力][分類モデルと比較][離散値を出力][「カンゾウ」や「トラユリ」など]",	    
       "[漢] [相關] [一種模型] [輸出連續（通常是浮點）值] [與分類模型比較] [輸出離散值] [比如‘day lily’或者‘tiger lily’]",
 ],
+ "[英] [related] "[和] [関連][kannrenn][かんれん] "[漢] [相關] 
+[A type of model] [モデルの一種][moderu no isshu][モデルのいっしゅ] [一種模型]
+[outputs continuous (typically, floating-point) values]. [連続した（通常は浮動小数点）値を出力][renzoku shita (tsuujou ha fudoushousuuten) atai wo syutsuryoku][れんぞくした（つうじょうはふどうしょうすうてん）あたいをしゅつりょく][輸出連續（通常是浮點）值]
+[Compare with classification models][分類モデルと比較][bunnrui moderu to hikaku][ぶんるいモデルとひかく][輸出連續（通常是浮點）值]
+[output discrete values] [離散値を出力][risanchi wo shytsuryoku][りさんちをしゅつりょく][輸出離散值]
+[such as 'day lily' or 'tiger lily']", [「カンゾウ」や「トラユリ」など][「wasuregusa」ya「oniyuri」nado][「わすれぐさ」や「オニユリ」など][比如‘day lily’或者‘tiger lily’]",
+      
+     
+],
