b137435db41ca194f2990d7124e98ff1292268f4
akiko-113 <104332806+akiko-113@users.noreply.github.com>
2022-06-14 16:49:14 +0900

    Update linear_model.js

--- tasks/tmp/linear_model.js
+++ tasks/tmp/linear_model.js
@@ -6,3 +6,11 @@
       "[和] [関連] [モデル] [特徴ごとに1つの重みを割り当てる] [予測を行うため] [線形モデルにもバイアスが組み込まれています]",	    
       "[漢] [相關] [一個模型] [為每個特徵分配一個權重] [進行預測] [線性模型也包含偏差]",
     ],
+
+      "[英] [related]  "[和] [関連][kanren][かんれん]"[漢] [相關]
+[A model] [モデル][moderu][モデル] [一個模型] 
+[ that assigns one weight per feature]  [特徴ごとに1つの重みを割り当てる][tokuchou goto ni hitotsu no omomi wo wariateru][とくちょうごとにひとつのおもみをわりあてる] [為每個特徵分配一個權重]
+[to make predictions]  [予測を行うため][yosoku wo okonau tameni][よそくをおこなうために] [進行預測] 
+[Linear models also incorporate a bias]",  [線形モデルにもバイアスが組み込まれている][senkei moderu nimo baiasu ga kumikomareteiru][せんけいモデルにもバイアスがくみこまれている]", [線性模型也包含偏差]",
+    	    
+     
