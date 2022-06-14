206807a0f1d709ba1d424a8775e051c2c919aa8b
akiko-113 <104332806+akiko-113@users.noreply.github.com>
2022-06-14 16:45:30 +0900

    Update classification_model.js

--- tasks/tmp/classification_model.js
+++ tasks/tmp/classification_model.js
@@ -4,3 +4,13 @@
       "[和] [関連] [機械学習モデルの一種] [2つ以上の個別のクラスを区別するため] [例] [自然言語処理] [分類モデル][入力文がフランス語, スペイン語, イタリア語のいずれであるかを判断できた] [回帰モデルと比較]",	    
       "[漢] [相關] [貝爾曼方程] [遵循特定的策略和功能] [給你] [最佳價值] [基於狀態] [遞歸計算] [通過將狀態/路徑劃分為子問題 或更小的問題]",
     ],
+
+        "[英] [related] "[和] [関連][kanren][かんれん]  "[漢] [相關] 
+    [A type of machine learning model]  [機械学習モデルの一種] [kikaigakushuu moderu no isshu][きかいがくしゅうモデルのいっしゅ]
+    [for distinguishing among two or more discrete classes] [2つ以上の個別のクラスを区別するため][futatsu ijou no kobetsu no kurasu wo kubetsusuru tame][ふたついじょうのこべつのクラスをくべつするため]  
+    [For example][例][rei][れい]
+    [a natural language processing]  [自然言語処理] [shizen gengo shori][しぜんげんごしょり]
+    [classification model] [分類モデル][bunnrui moderu][ぶんるいモデル]
+    [could determine whether an input sentence] [入力文がいずれであるかを判断できた][nyuuryokubun ga izuredearuka handan dekita][にゅうりょくぶんがいずれであるかはんだんできた]
+    [was in French, Spanish, or Italian][フランス語, スペイン語, イタリア語の][furannsu go, supein go, itaria go no][スランスご、スペインご、イタリアごの]
+    [Compare with regression model]", [回帰モデルと比較][kaiki moderu to hikaku][かいきモデルとひかく]
