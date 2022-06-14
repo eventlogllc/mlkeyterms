4c8aaae8ddf1a3fde2a87b1df5f5c55ea4e453f6
akiko-113 <104332806+akiko-113@users.noreply.github.com>
2022-06-14 17:49:34 +0900

    Update logistic_regression.js

--- tasks/tmp/logistic_regression.js
+++ tasks/tmp/logistic_regression.js
@@ -9,3 +9,24 @@
       "[和] [関連] [分類モデル][シグモイド関数を使用して線形モデルの生の予測（y'）を変換する][0から1の間の値に変換する][0から1の間の値を解釈できます][次の2つのいずれかで 方法：] [- 確率として][例が正のクラスに属すること][バイナリ分類問題で] [- 分類しきい値と比較する値として][値が分類しきい値以上の場合][システムは例を正のクラスとして分類します][逆に][値が指定されたしきい値を下回っている場合] [システムは例をネガティブクラスとして分類します]",	    
       "[漢] [相關] [一個分類模型] [使用sigmoid函數將線性模型的原始預測（y'）] [轉換為0到1之間的值] [可以解釋0到1之間的值] [以下兩種 方法：] [- 作為概率] [示例屬於正類] [在二元分類問題中] [- 作為要與分類閾值進行比較的值] [如果該值等於或高於分類閾值] [系統將示例分類為正類] [相反] [如果該值低於給定閾值] [ 系統將示例分類為負類]",
   ]
+
+"[英] [related] "[和] [関連]"[漢] [相關]
+[A classification model] [分類モデル][bunrui moderu][ぶんるいモデル] [一個分類模型] 
+[that uses a sigmoid function to convert a linear model's raw prediction (y')into a value between 0 and 1]
+[シグモイド関数を使用して線形モデルの生の予測（y'）を0から1の間の値に変換する][sigumoido kansuu wo shiyoushite senkei moderu no namano yosoku (y')wo 0 kara 1 no aida no atai ni henkannsuru]
+[シグモイドかんすうをしようしてせんけいモデルのなまのよそく(y')を０から１のあいだのあたいにへんかんする]
+ [使用sigmoid函數將線性模型的原始預測（y'）] [轉換為0到1之間的值] 
+[You can interpret the value between 0 and 1][0から1の間の値を解釈できる][0 kara 1 no aida no atai wo kaisyaku dekiru][０から１のあいだのあたいをかいしゃくできる][可以解釋0到1之間的值]
+[in either of the following two ways:][次の2つのいずれかの方法で][tsugi no futatsu no izurekano houhou de][つぎのふたるのいずれかのほうほうで][以下兩種 方法：]
+[As a probability] [- 確率として][kakuritsu toshite][かくりつとして] [- 作為概率]
+[that the example belongs to the positive class]  [例が正のクラスに属すること] [rei ga sei no kurasu ni zokusuru koto][れいがせいのクラスにぞくすること][示例屬於正類] 
+[in a binary classification problem] [バイナリ分類問題で] [bainari bunnrui mondai de][バイナリぶんるいもんだいで] [在二元分類問題中]
+[As a value to be compared against a classification threshold] [- 分類しきい値と比較する値として][bunnrui shikiichi to hikakusuru atai toshite][ぶんるいしきいちとひかくするあたいとして][- 作為要與分類閾值進行比較的值] 
+[If the value is equal to or above the classification threshold]  [値が分類しきい値以上の場合][atai ga bunnrui shikiichi ijou no baai][あたいがぶんるいしきいちいじょうのばあい][如果該值等於或高於分類閾值]
+[the system classifies the example as the positive class]  [システムは例を正のクラスとして分類する][shisutemu wa rei wo sei no kurasu toshite bunnruisuru][システムはれいをせいのクラスとしてぶんるいする][系統將示例分類為正類]
+[Conversely] [逆に][gyakuni][ぎゃくに][相反] 
+[if the value is below the given threshold] [値が指定されたしきい値を下回っている場合] [atai ga shiteisareta shikiichi wo shitamawatteiru baai][あたいがしていされたしきいちをしたまわっているばあい][如果該值低於給定閾值]
+[the system classifies the example as the negative class] [システムは例をネガティブクラスとして分類する][shisutemu wa rei wo negatibu kurasu toshite bunnruisuru][システムはれいをネガティブクラスとしてぶんるいする] [ 系統將示例分類為負類]
+  ",	    
+       ",
+  ]
