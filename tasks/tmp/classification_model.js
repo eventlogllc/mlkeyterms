e04c50935921be9e6d1dddce84ea24a111a2851d
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-06-14 22:08:10 -0700

    Update classification_model.js

--- tasks/tmp/classification_model.js
+++ tasks/tmp/classification_model.js
@@ -2,15 +2,15 @@
       "A type of machine learning model for distinguishing among two or more discrete classes. For example, a natural language processing classification model could determine whether an input sentence was in French, Spanish, or Italian. Compare with regression model.",
       "[英] [related] [A type of machine learning model] [for distinguishing among two or more discrete classes] [For example] [a natural language processing] [classification model] [could determine whether an input sentence] [was in French, Spanish, or Italian] [Compare with regression model]",
       "[和] [関連] [機械学習モデルの一種] [2つ以上の個別のクラスを区別するため] [例] [自然言語処理] [分類モデル][入力文がフランス語, スペイン語, イタリア語のいずれであるかを判断できた] [回帰モデルと比較]",	    
-      "[漢] [相關] [貝爾曼方程] [遵循特定的策略和功能] [給你] [最佳價值] [基於狀態] [遞歸計算] [通過將狀態/路徑劃分為子問題 或更小的問題]",
+      "[漢] [相關] [一種機器學習模型] [用於區分兩個或多個離散類] [例如] [自然語言處理] [分類模型] [可以確定輸入句子] [是法語、西班牙語還是意大利語] 【與回歸模型比較】",
     ],
 
         "[英] [related] "[和] [関連][kanren][かんれん]  "[漢] [相關] 
-    [A type of machine learning model]  [機械学習モデルの一種] [kikaigakushuu moderu no isshu][きかいがくしゅうモデルのいっしゅ]
-    [for distinguishing among two or more discrete classes] [2つ以上の個別のクラスを区別するため][futatsu ijou no kobetsu no kurasu wo kubetsusuru tame][ふたついじょうのこべつのクラスをくべつするため]  
-    [For example][例][rei][れい]
-    [a natural language processing]  [自然言語処理] [shizen gengo shori][しぜんげんごしょり]
-    [classification model] [分類モデル][bunnrui moderu][ぶんるいモデル]
-    [could determine whether an input sentence] [入力文がいずれであるかを判断できた][nyuuryokubun ga izuredearuka handan dekita][にゅうりょくぶんがいずれであるかはんだんできた]
-    [was in French, Spanish, or Italian][フランス語, スペイン語, イタリア語の][furannsu go, supein go, itaria go no][スランスご、スペインご、イタリアごの]
-    [Compare with regression model]", [回帰モデルと比較][kaiki moderu to hikaku][かいきモデルとひかく]
+    [A type of machine learning model]  [機械学習モデルの一種] [kikaigakushuu moderu no isshu][きかいがくしゅうモデルのいっしゅ] [一種機器學習模型]
+    [for distinguishing among two or more discrete classes] [2つ以上の個別のクラスを区別するため][futatsu ijou no kobetsu no kurasu wo kubetsusuru tame][ふたついじょうのこべつのクラスをくべつするため]  [用於區分兩個或多個離散類] 
+    [For example][例][rei][れい] [例如]
+    [a natural language processing]  [自然言語処理] [shizen gengo shori][しぜんげんごしょり][自然語言處理] 
+    [classification model] [分類モデル][bunnrui moderu][ぶんるいモデル] [分類模型] 
+    [could determine whether an input sentence] [入力文がいずれであるかを判断できた][nyuuryokubun ga izuredearuka handan dekita][にゅうりょくぶんがいずれであるかはんだんできた] [可以確定輸入句子]
+    [was in French, Spanish, or Italian][フランス語, スペイン語, イタリア語の][furannsu go, supein go, itaria go no][スランスご、スペインご、イタリアごの] [是法語、西班牙語還是意大利語]
+    [Compare with regression model]", [回帰モデルと比較][kaiki moderu to hikaku][かいきモデルとひかく] 【與回歸模型比較】
