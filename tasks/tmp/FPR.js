5bf151855539ecbbbf3c6c8b7f1ed4a7ad6b30a8
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-06-18 15:02:41 -0700

    Update FPR.js

--- tasks/tmp/FPR.js
+++ tasks/tmp/FPR.js
@@ -3,5 +3,5 @@
 false positive rate (FPR)
 The x-axis in an ROC curve. The false positive rate is defined as follows:
 
-fpr = FP / (FP + TN)
+The false positive rate is {\displaystyle FPR={\frac {\mathrm {FP} }{\mathrm {FP} +\mathrm {TN} }}}{\displaystyle FPR={\frac {\mathrm {FP} }{\mathrm {FP} +\mathrm {TN} }}}
 where {\displaystyle \mathrm {FP} }{\displaystyle \mathrm {FP} } is the number of false positives, {\displaystyle \mathrm {TN} }{\displaystyle \mathrm {TN} } is the number of true negatives and {\displaystyle N=\mathrm {FP} +\mathrm {TN} }{\displaystyle N=\mathrm {FP} +\mathrm {TN} } is the total number of ground truth negatives.
