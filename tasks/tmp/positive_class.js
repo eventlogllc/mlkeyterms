6f38484dd9acc0702d8d726392db3ea7cbbfa20b
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-06-18 14:19:38 -0700

    Create positive_class.js

---
+++ tasks/tmp/positive_class.js
@@ -0,0 +1,10 @@
+"what": [
+      "In binary classification, the two possible classes are labeled as positive and negative.",
+      "The positive outcome is the thing we're testing for. (Admittedly, we're simultaneously testing for both outcomes, but play along.)",
+      "For example, the positive class in a medical test might be 'tumor.' The positive class in an email classifier might be 'spam.'",
+      "Contrast with negative class.",
+      "[英] [related] [In binary classification] [the two possible classes are labeled as positive and negative] [The positive outcome is the thing we're testing for] [Admittedly] [we're simultaneously testing for both outcomes] [For example] [the positive class in a medical test] [might be 'tumor'] [The positive class in an email classifier] [might be 'spam'] [Contrast with negative class]",
+      "[和] [関連] [在二元分類中] [兩個可能的類別被標記為正面和負面] [正面結果是我們正在測試的東西] [誠然] [我們同時測試兩個結果] [例如] [正面 醫學測試中的類別] [可能是“腫瘤”] [電子郵件分類器中的陽性類別] [可能是“垃圾郵件”] [與陰性類別對比]",	    
+      "[漢] [相關] [二項分類で] [2つの可能なクラスはポジティブとネガティブとしてラベル付けされます][ポジティブな結果は私たちがテストしているものです][確かに][私たちは両方の結果を同時にテストしています][例][ポジティブ 医療検査のクラス][「腫瘍」の可能性があります][電子メール分類子の陽性クラス][「スパム」の可能性があります][陰性クラスとの対比]",
+    ],
+
