45c76ddb3e173b3176f2f3673f1b554554811341
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-06-18 14:32:40 -0700

    Create precision.js

---
+++ tasks/tmp/precision.js
@@ -0,0 +1,10 @@
+"what": [
+      "A metric for classification models. Precision identifies the frequency with which a model was correct when predicting the positive class.",
+      "precision = true_positive / ( true_positive + false_posive )",
+      "[英] [related] The Bellman Equation, follows a specific policy and function, to give you, the best value, based on the states, it calculated recursively through dividing the states/paths up into subproblems or smaller problems. ",
+      "[和] [関連] [ベルマン方程式], [特定のポリシーと機能に従う], [与える], [最良の値], [状態に基づく], [再帰的に計算する], [状態/パスをサブ問題に分割することにより または小さな問題]",	    
+      "[漢] [相關] [貝爾曼方程], [遵循特定的策略和功能], [給你], [最佳價值], [基於狀態], [遞歸計算], [通過將狀態/路徑劃分為子問題 或更小的問題]",
+    ],
+
+precision
+A metric for classification models. Precision identifies the frequency with which a model was correct when predicting the positive class. 
