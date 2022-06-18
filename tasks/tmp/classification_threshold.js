ff16a6597f5479ab3a58bf6956ffda27d608427e
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-06-18 14:06:06 -0700

    Update classification_threshold.js

--- tasks/tmp/classification_threshold.js
+++ tasks/tmp/classification_threshold.js
@@ -5,6 +5,6 @@
       "If the classification threshold is 0.9,",
       "then logistic regression values above 0.9 are classified as spam and those below 0.9 are classified as not spam.",
       "[英] [related] [A scalar-value criterion] [that is compared to a model's predicted score] [in order to] [separate the positive class from the negative class] [Used when mapping logistic regression results to binary classification]",
-      "[和] [関連] [スカラー値基準][モデルの予測スコアと比較されます][目的][正のクラスを負のクラスから分離する][ロジスティック回帰の結果を2項分類にマッピングするときに使用されます]",	    
+      "[和] [関連] [スカラー値基準] [モデルの予測スコアと比較されます][目的][正のクラスを負のクラスから分離する] [ロジスティック回帰の結果を2項分類にマッピングするときに使用されます]",	    
       "[漢] [相關] [標量值標準] [與模型的預測分數進行比較] [為了] [將正類與負類分開] [在將邏輯回歸結果映射到二元分類時使用]",
     ],
