af0f04cb437184ea08f7af6c26c0c8f75f5fb6d2
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-06-18 14:12:57 -0700

    Create binary_classification.js

---
+++ tasks/tmp/binary_classification.js
@@ -0,0 +1,9 @@
+"what": [
+      "A type of classification task that outputs one of two mutually exclusive classes.",
+      "For example, a machine learning model that evaluates email messages and outputs either 'spam' or 'not spam' is a binary classifier.",
+      "Contrast with multi-class classification.",
+      "[英] [related] [A type of classification task that outputs one of two mutually exclusive classes] [For example] [a machine learning model] [that evaluates email messages and outputs either 'spam' or 'not spam'] [is a binary classifier]  [Contrast with multi-class classification]",
+      "[和] [関連] [2つの相互に排他的なクラスの1つを出力する分類タスクのタイプ][例][機械学習モデル][電子メールメッセージを評価し、「スパム」または「非スパム」のいずれかを出力する][バイナリ分類器][コントラスト マルチクラス分類あり] ",	    
+      "[漢] [相關] [一種輸出兩個互斥類別之一的分類任務] [例如] [機器學習模型] [評估電子郵件並輸出“垃圾郵件”或“非垃圾郵件”] [是二元分類器] [對比 具有多類分類] ",
+    ],
+    
