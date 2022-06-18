95b502c9be75d2158bd34c089d79fa57d4a5bfa9
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-06-18 14:52:33 -0700

    Update recall.js

--- tasks/tmp/recall.js
+++ tasks/tmp/recall.js
@@ -1,7 +1,7 @@
 "what": [
       "A metric for classification models that answers the following question:",
       "Out of all the possible positive labels, how many did the model correctly identify?",
-      "recall = true-positive / ( true-positive + false-negative )",
+      "recall = true-positives / ( true-positives + false-negatives )",
       "[英] [related] [a] [metric for classification models] [that answers the following question] [Out of all the possible positive labels] [how many did the model correctly identify?] [recall] [=] [true-positive] [/] [( true-positive + false-negative )]",
       "[和] [関連][一種の][分類モデルの測定基準][次の質問に答える][考えられるすべての陽性ラベルのうち][モデルが正しく識別した数][再現率][=][真陽性][/ ] [（true-positive + false-negative）]",	    
       "[漢] [相關] [一種] [分類模型的指標] [回答以下問題] [在所有可能的正面標籤中] [模型正確識別了多少？] [回憶] [=] [真陽性] [/ ] [( 真陽性 + 假陰性 )]",
