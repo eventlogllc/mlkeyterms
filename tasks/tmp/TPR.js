431bee3f9a3d21c5422de1355743f2d0f16d57df
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-06-18 15:05:00 -0700

    Update TPR.js

--- tasks/tmp/TPR.js
+++ tasks/tmp/TPR.js
@@ -4,3 +4,4 @@ true positive rate (TPR)
 Synonym for recall. That is:
 
 True positive rate is the y-axis in an ROC curve.
+{\displaystyle \mathrm {TPR} ={\frac {\mathrm {TP} }{\mathrm {P} }}={\frac {\mathrm {TP} }{\mathrm {TP} +\mathrm {FN} }}=1-\mathrm {FNR} }
