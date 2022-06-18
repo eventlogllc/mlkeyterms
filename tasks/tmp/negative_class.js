09e60f3978aaf07cc20d46809fd8d5892198ec9d
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-06-18 14:27:13 -0700

    Update negative_class.js

--- tasks/tmp/negative_class.js
+++ tasks/tmp/negative_class.js
@@ -2,5 +2,5 @@
       "In binary classification, one class is termed positive and the other is termed negative. The positive class is the thing we're looking for and the negative class is the other possibility. For example, the negative class in a medical test might be 'not tumor' The negative class in an email classifier might be ]not spam.' See also positive class.",
       "[英] [related] [In binary classification] [one class is termed positive] [and the other is termed negative] [The positive class is the thing we're looking for] [and the negative class is the other possibility] [For example] [the negative class in a medical test] [might be 'not tumor'] [The negative class in an email classifier] [might be 'not spam] [See also positive class]",
       "[和] [関連] [二項分類では][一方のクラスはポジティブと呼ばれます][もう一方はネガティブと呼ばれます][ポジティブクラスは私たちが探しているものです][そしてネガティブクラスは他の可能性です][たとえば][ネガティブ 医療検査のクラス][腫瘍ではない可能性があります][電子メール分類子の陰性クラス][スパムではない可能性があります][陽性クラスも参照]",	    
-      "[漢] [相關] [在二元分類中] [一類被稱為正類] [另一個被稱為負類] [正類是我們正在尋找的東西] [負類是另一種可能性] [例如] [負 醫學測試中的類] [可能是“不是腫瘤”] [電子郵件分類器中的負類] [可能是'不是垃圾郵件'[另見正類]",
+      "[漢] [相關] [在二元分類中] [一類被稱為正類] [另一個被稱為負類] [正類是我們正在尋找的東西] [負類是另一種可能性] [例如] [負 醫學測試中的類] [可能是“不是腫瘤”] [電子郵件分類器中的負類] [可能是'不是垃圾郵件'] [另見正類]",
     ],
