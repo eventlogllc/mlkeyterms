fda1f1f94cd8f23ccc78039eb2eb7f83424f562d
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-06-18 14:49:23 -0700

    Update recall.js

--- tasks/tmp/recall.js
+++ tasks/tmp/recall.js
@@ -2,9 +2,9 @@
       "A metric for classification models that answers the following question:",
       "Out of all the possible positive labels, how many did the model correctly identify?",
       "recall = true-positive / ( true-positive + false-negative )",
-      "[英] [related] The Bellman Equation, follows a specific policy and function, to give you, the best value, based on the states, it calculated recursively through dividing the states/paths up into subproblems or smaller problems. ",
-      "[和] [関連] [ベルマン方程式], [特定のポリシーと機能に従う], [与える], [最良の値], [状態に基づく], [再帰的に計算する], [状態/パスをサブ問題に分割することにより または小さな問題]",	    
-      "[漢] [相關] [貝爾曼方程], [遵循特定的策略和功能], [給你], [最佳價值], [基於狀態], [遞歸計算], [通過將狀態/路徑劃分為子問題 或更小的問題]",
+      "[英] [related] [a] [metric for classification models] [that answers the following question] [Out of all the possible positive labels] [how many did the model correctly identify?] [recall] [=] [true-positive] [/] [( true-positive + false-negative )]",
+      "[和] [関連][一種の][分類モデルの測定基準][次の質問に答える][考えられるすべての陽性ラベルのうち][モデルが正しく識別した数][再現率][=][真陽性][/ ] [（true-positive + false-negative）]",	    
+      "[漢] [相關] [一種] [分類模型的指標] [回答以下問題] [在所有可能的正面標籤中] [模型正確識別了多少？] [回憶] [=] [真陽性] [/ ] [( 真陽性 + 假陰性 )]",
     ],
   
 recall
