aa33397fa8b8f341d2c113d2f66bab33409017ba
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-06-18 14:54:38 -0700

    Create AUC.js

---
+++ tasks/tmp/AUC.js
@@ -0,0 +1,6 @@
+
+
+AUC (Area under the ROC Curve)
+An evaluation metric that considers all possible classification thresholds.
+
+The Area Under the ROC curve is the probability that a classifier will be more confident that a randomly chosen positive example is actually positive than that a randomly chosen negative example is positive.
