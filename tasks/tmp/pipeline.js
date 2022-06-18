d16c6821497c3c1f0b8a7597a3a2e1b47db2dea0
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-06-18 13:54:43 -0700

    Create pipeline.js

---
+++ tasks/tmp/pipeline.js
@@ -0,0 +1,7 @@
+"what": [
+      "The infrastructure surrounding a machine learning algorithm. A pipeline includes gathering the data, putting the data into training data files, training one or more models, and exporting the models to production.",
+      "[英] [related] [The infrastructure] [surrounding] [a machine learning algorithm] [A pipeline] [includes] [gathering the data] [putting the data into training data files] [training one or more models] [and exporting the models to production] ",
+      "[和] [関連] [インフラストラクチャ][周囲][機械学習アルゴリズム][パイプライン][含む][データの収集][データをトレーニングデータファイルに入れる][1つ以上のモデルをトレーニングする][モデルを本番環境にエクスポートする]",	    
+      "[漢] [相關] [基礎設施] [周邊] [機器學習算法] [管道] [包括] [收集數據] [將數據放入訓練數據文件] [訓練一個或多個模型] [並將模型導出到生產環境]",
+    ],
+    
