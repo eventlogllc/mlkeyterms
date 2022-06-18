d6f1eb5a8caca36bda0f3e7cb89ce0065e79c792
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-06-18 11:12:48 -0700

    Create convolution.js

---
+++ tasks/convolution.js
@@ -0,0 +1,8 @@
+    "what": [
+      "In mathematics, casually speaking, a mixture of two functions. In machine learning, a convolution mixes the convolutional filter and the input matrix in order to train weights.",
+      "The term 'convolution' in machine learning is often a shorthand way of referring to either convolutional operation or convolutional layer.",
+      "Without convolutions, a machine learning algorithm would have to learn a separate weight for every cell in a large tensor.  For example, a machine learning algorithm training on 2K x 2K images would be forced to find 4M separate weights. Thanks to convolutions, a machine learning algorithm only has to find weights for every cell in the convolutional filter, dramatically reducing the memory needed to train the model.  When the convolutional filter is applied, it is simply replicated across cells such that each is multiplied by the filter.",
+      "[英] [related] The Bellman Equation, follows a specific policy and function, to give you, the best value, based on the states, it calculated recursively through dividing the states/paths up into subproblems or smaller problems. ",
+      "[和] [関連] [ベルマン方程式], [特定のポリシーと機能に従う], [与える], [最良の値], [状態に基づく], [再帰的に計算する], [状態/パスをサブ問題に分割することにより または小さな問題]",	    
+      "[漢] [相關] [貝爾曼方程], [遵循特定的策略和功能], [給你], [最佳價值], [基於狀態], [遞歸計算], [通過將狀態/路徑劃分為子問題 或更小的問題]",
+    ],
