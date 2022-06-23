92794ef9fb131fd8f6e59f9f3201e28630c29775
akiko-113 <104332806+akiko-113@users.noreply.github.com>
2022-06-23 21:49:08 +0900

    Update binary_classification.js

--- tasks/tmp/binary_classification.js
+++ tasks/tmp/binary_classification.js
@@ -7,3 +7,12 @@
       "[漢] [相關] [一種輸出兩個互斥類別之一的分類任務] [例如] [機器學習模型] [評估電子郵件並輸出“垃圾郵件”或“非垃圾郵件”] [是二元分類器] [對比 具有多類分類] ",
     ],
     
+[英] [related]"[和] [関連][かんれん][kannrenn][漢] [相關]
+[A type of classification task that outputs one of two mutually exclusive classes][2つの相互に排他的なクラスの1つを出力する分類タスクのタイプ][ふたつのそうごにはいたてきなクラスのひとつをしゅつりょくするぶんるいタスクのタイプ][futatsu no sougo ni haitatekin akurasu no hitotsu wo shutsuryokusuru bunrui tasuku no taipu][一種輸出兩個互斥類別之一的分類任務]
+[For example]  [例][れい][rei][例如] 
+[a machine learning model] [機械学習モデル] [きかいがくしゅうモデル][kikai gakushu moderu][機器學習模型] 
+[that evaluates email messages and outputs either 'spam' or 'not spam'] [電子メールメッセージを評価し、「スパム」または「非スパム」のいずれかを出力する][でんしメールメッセージをひょうかし、「スパム」または「ひスパム」のいずれかをしゅつりょくする][denshi me-ru messeigi wo hyoukashi, 「supamu」matawa「hi-sipamu」no izurekawo shutsuryoku suru][評估電子郵件並輸出“垃圾郵件”或“非垃圾郵件”]
+[is a binary classifier]  [バイナリ分類器][バイナリぶんるいき][bainari bunnruiki][是二元分類器]
+[Contrast with multi-class classification] [多クラス分類との対比][たクラスぶんるいとのたいひ][takurasu bunnrui tono taihi][對比 具有多類分類] 
+     ",	    
+      "  
