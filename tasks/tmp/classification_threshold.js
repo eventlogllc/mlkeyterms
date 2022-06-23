93ebd5023f1e97ef40c18d7d2daa61049f2a7eae
akiko-113 <104332806+akiko-113@users.noreply.github.com>
2022-06-23 21:56:20 +0900

    Update classification_threshold.js

--- tasks/tmp/classification_threshold.js
+++ tasks/tmp/classification_threshold.js
@@ -8,3 +8,9 @@
       "[和] [関連] [スカラー値基準] [モデルの予測スコアと比較されます][目的][正のクラスを負のクラスから分離する] [ロジスティック回帰の結果を2項分類にマッピングするときに使用されます]",	    
       "[漢] [相關] [標量值標準] [與模型的預測分數進行比較] [為了] [將正類與負類分開] [在將邏輯回歸結果映射到二元分類時使用]",
     ],
+[英] [related] [和] [関連][かんれん][kannrenn] [漢] [相關] 
+[A scalar-value criterion] [スカラー値基準][スカラーちきじゅん][sukara-chi kijun]  [標量值標準] 
+[that is compared to a model's predicted score] [モデルの予測スコアと比較される] [モデルのよそくスコアとひかくされる][moderu no yosokusukoa to hikakusareru][與模型的預測分數進行比較] 
+ [in order to separate the positive class from the negative class][正のクラスを負のクラスから分離するため][せいのクラスをふのクラスからぶんりするため][seino kurasu wo funo kurasu kara bunri surutame][為了] [將正類與負類分開] 
+[Used when mapping logistic regression results to binary classification]
+[ロジスティック回帰の結果を2項分類にマッピングするときに使用される][ロジステックかいきのけっかを２こうぶんるいにマッピングするときにしようされる][rojisutikkukaiji no kekka wo nikoubunnrui ni mappingu surutokini shiyousareru][在將邏輯回歸結果映射到二元分類時使用]",
