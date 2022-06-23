d8c0ed866ca9ab51f3c4d34b18c08d02fe12d88c
akiko-113 <104332806+akiko-113@users.noreply.github.com>
2022-06-23 22:36:33 +0900

    Update positive_class.js

--- tasks/tmp/positive_class.js
+++ tasks/tmp/positive_class.js
@@ -8,3 +8,15 @@
       "[漢] [相關] [二項分類で] [2つの可能なクラスはポジティブとネガティブとしてラベル付けされます][ポジティブな結果は私たちがテストしているものです][確かに][私たちは両方の結果を同時にテストしています][例][ポジティブ 医療検査のクラス][「腫瘍」の可能性があります][電子メール分類子の陽性クラス][「スパム」の可能性があります][陰性クラスとの対比]",
     ],
 
+[英] [related] [和] [関連][漢][相關]
+[In binary classification][二項分類で][にこうぶんるいで][nikou bunrui de] [在二元分類中]
+[the two possible classes are labeled as positive and negative][2つの可能なクラスは陽性と陰性としてラベル付けされる][ふたつのかのうなクラスはようせいといんせいとしてラベルづけされる][futatsu no kanou na kurasu wa yousei to insei toshite raberu duke sareru] [兩個可能的類別被標記為正面和負面] 
+[The positive outcome is the thing we're testing for][陽性の結果は私たちがテストしているものである][ようせいのけっかはわたしたちがテストしているものである][yousei no kekka wa watashitachi ga tesuto shiteiru monodearu][正面結果是我們正在測試的東西]
+ [Admittedly][確かに][たしかに][tashikani] [誠然]
+[we're simultaneously testing for both outcomes][私たちは両方の結果を同時にテストしている][わたしたちはりょうほうのけっかをどうじにテストしている][watashitachi was ryouhou no kekka wo douji ni tesuto shiteiru][我們同時測試兩個結果]
+ [For example][例][れい][rei][例如]
+[the positive class in a medical test][医療検査の陽性クラス][いりょうけんさのようせいクラス][iryoukensa no yousei kensa][正面 醫學測試中的類別]
+[might be 'tumor'][「腫瘍」の可能性がある][しゅようのかのうせいがある][syuyou no kanousei gaaru][可能是“腫瘤”]
+[The positive class in an email classifier][電子メール分類子の陽性クラス][でんしメールぶんるいしのようせいクラス][denshi meiru buruishi no yousei kurasu][電子郵件分類器中的陽性類別]
+[might be 'spam'][「スパム」の可能性がある][スパムのかのうせいがある][supamu no kanousei gaaru][可能是“垃圾郵件”]
+[Contrast with negative class][陰性クラスとの対比][いんせいクラスとのたいひ][insei kurasu tono taihi][與陰性類別對比]
