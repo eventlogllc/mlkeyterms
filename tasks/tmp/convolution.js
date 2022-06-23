67b5b8783528b528faff8f604edeaa89e6c5dc0f
akiko-113 <104332806+akiko-113@users.noreply.github.com>
2022-06-23 22:04:23 +0900

    Update convolution.js

--- tasks/tmp/convolution.js
+++ tasks/tmp/convolution.js
@@ -6,3 +6,11 @@
       "[和] [関連] [畳み込み][数学の場合][2つの関数の混合][機械学習の場合][畳み込みは畳み込みフィルターを混合します][および入力行列][重みをトレーニングするため]",	    
       "[漢] [相關] [卷積] [在數學中] [兩個函數的混合] [在機器學習中] [卷積混合卷積濾波器] [和輸入矩陣] [為了訓練權重]",
     ],
+
+        [英] [related] [和] [関連][かんれん][kannrenn][漢] [相關]
+    [convolution] [畳み込み][たたみこみ][tatamikomi][卷積]
+    [In mathematics] [数学の場合][すうがくのばあい][suugakuno baai] [在數學中] 
+    [a mixture of two functions] [2つの関数の混合][ふたつのかんすうのこんごう][futatsu no kannsuu nokongou][兩個函數的混合] 
+    [In machine learning][機械学習の場合][きかいがくしゅうのばあい][kikaigakushu no baai][在機器學習中]
+    [a convolution mixes the convolutional filter and the input matrix][畳み込みは畳み込みフィルターと入力行列を混合する][たたみこみはたたみこみフィルターとにゅうりょくぎょうれつをこんごうする][tatamikomi wa tatamikomi firuta- to nyuuryoku gyouretsu wo kongousuru][卷積混合卷積濾波器] [和輸入矩陣]
+[in order to train weights][重みをトレーニングするため][おもみをトレーニングするため][omomi wo treiningu surutame][為了訓練權重]
