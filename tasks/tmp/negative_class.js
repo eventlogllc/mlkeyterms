ddc69f1d3c0e47132ff161bca9fdb8cd642db67c
akiko-113 <104332806+akiko-113@users.noreply.github.com>
2022-06-23 22:19:15 +0900

    Update negative_class.js

--- tasks/tmp/negative_class.js
+++ tasks/tmp/negative_class.js
@@ -4,3 +4,15 @@
       "[和] [関連] [二項分類では][一方のクラスはポジティブと呼ばれます][もう一方はネガティブと呼ばれます][ポジティブクラスは私たちが探しているものです][そしてネガティブクラスは他の可能性です][たとえば][ネガティブ 医療検査のクラス][腫瘍ではない可能性があります][電子メール分類子の陰性クラス][スパムではない可能性があります][陽性クラスも参照]",	    
       "[漢] [相關] [在二元分類中] [一類被稱為正類] [另一個被稱為負類] [正類是我們正在尋找的東西] [負類是另一種可能性] [例如] [負 醫學測試中的類] [可能是“不是腫瘤”] [電子郵件分類器中的負類] [可能是'不是垃圾郵件'] [另見正類]",
     ],
+[英] [related] [和] [関連][かんれん][kanren][漢] [相關]
+[In binary classification][二項分類では][にこうぶんるいでは][nikoubunrui dewa][在二元分類中]
+[one class is termed positive]  [一方のクラスは陽性と呼ばれる][いっぽうのクラスはようせいとよばれる][ippou no kurasu ha yousei to yobareru][一類被稱為正類]
+[and the other is termed negative] [もう一方は陰性と呼ばれる]いっぽうのクラスはいんせいとよばれる][ippou no kurasu ha insei to yobareru][另一個被稱為負類]
+[The positive class is the thing we're looking for] [陽性クラスは私たちが探しているものである][ようせいクラスはわたしたちがさがしているものである][yousei kurasu wa watashitatiga sagashiteirumonodearu] [正類是我們正在尋找的東西]
+ [and the negative class is the other possibility][そして陰性クラスは他の可能性である][そしていんせいクラスはほかのかのうせいである][soshite, insei kurasu wa hokano kanousei dearu] [負類是另一種可能性]
+[For example] [たとえば][たとえば][tatoeba][例如] 
+[the negative class in a medical test][医療検査の陰性クラス] [いりょうけんさのいんせいクラス][iryou kurasu no insei kurasu][負 醫學測試中的類]
+[might be 'not tumor'] [腫瘍ではない可能性がある][しゅようではないかのうせいがある][shuyou dewanai kanousei ga aru] [可能是“不是腫瘤”]
+[The negative class in an email classifier] [電子メール分類子の陰性クラス][でんしメールぶんるいしのいんせいクラス][denshi meiru bunruishi no insei kurasu][電子郵件分類器中的負類]
+[might be 'not spam][スパムではない可能性がある][スパムではないかのうせいがある][supamu dewa nai kanousei ga aru] [可能是'不是垃圾郵件']
+ [See also positive class][陽性クラスも参照][ようせいクラスもさんしょう][yousei kurasu mo sanshou][另見正類]
