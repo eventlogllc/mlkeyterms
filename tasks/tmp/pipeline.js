70b59acea6513bf2e2fbd691e5f6410cd282229a
akiko-113 <104332806+akiko-113@users.noreply.github.com>
2022-06-23 22:25:57 +0900

    Update pipeline.js

--- tasks/tmp/pipeline.js
+++ tasks/tmp/pipeline.js
@@ -5,3 +5,14 @@
       "[漢] [相關] [基礎設施] [周邊] [機器學習算法] [管道] [包括] [收集數據] [將數據放入訓練數據文件] [訓練一個或多個模型] [並將模型導出到生產環境]",
     ],
     
+"[英] [related] [和] [関連][かんれん][kanren][漢] [相關]
+[The infrastructure][インフラストラクチャ][インフラストラクチャ][infurasutorakutyaa][基礎設施]
+[surrounding] [周囲][しゅうい][shuui][周邊]
+[a machine learning algorithm] [機械学習アルゴリズム][きかいがくしゅうアルゴリズム][kikai gakushu arugorizumu][機器學習算法]
+[A pipeline] [パイプライン][パイプライン][paipu rain][管道]
+[includes] [含む][ふくむ][fukumu][包括]
+[gathering the data][データの収集][收集數據] 
+[putting the data into training data files] [データをトレーニングデータファイルに入れる][データをトレーニングファイルにいれる][deita wo toreiningu deita fairu ni ireru][將數據放入訓練數據文件] 
+[training one or more models][1つ以上のモデルをトレーニングする][ひとついじょうのモデルをトレーニングする][hitotsu ijou no moderu wo toreiningu suru][訓練一個或多個模型]
+[and exporting the models to production][モデルを本番環境にエクスポートする][モデルをほんばんかんきょにエクスポートする][moderu wo honban kankyuou ni ekusupo-to suru[並將模型導出到生產環境]
+  
