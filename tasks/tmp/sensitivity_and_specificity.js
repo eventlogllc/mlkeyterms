63eedd07125b333df074cb61feb5e5b14e0ff40c
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-06-25 10:55:06 -0700

    Create sensitivity_and_specificity.js

---
+++ tasks/tmp/sensitivity_and_specificity.js
@@ -0,0 +1,10 @@
+Sensitivity and specificity mathematically describe the accuracy of a test which reports the presence or absence of a condition. Individuals for which the condition is satisfied are considered "positive" and those for which it is not are considered "negative".
+
+Sensitivity (true positive rate) refers to the probability of a positive test, conditioned on truly being positive.
+Specificity (true negative rate) refers to the probability of a negative test, conditioned on truly being negative.
+
+True positive: Sick people correctly identified as sick
+False positive: Healthy people incorrectly identified as sick
+True negative: Healthy people correctly identified as healthy
+False negative: Sick people incorrectly identified as healthy
+
