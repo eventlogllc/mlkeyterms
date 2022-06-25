3d4dcd46d9990d8d7a67aac3bb3a199401acb65d
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-06-25 11:24:29 -0700

    Create true_false_positive_negative.js

---
+++ tasks/tmp/true_false_positive_negative.js
@@ -0,0 +1,14 @@
+
+
+condition positive (P)
+the number of real positive cases in the data
+condition negative (N)
+the number of real negative cases in the data
+true positive (TP)
+A test result that correctly indicates the presence of a condition or characteristic
+true negative (TN)
+A test result that correctly indicates the absence of a condition or characteristic
+false positive (FP)
+A test result which wrongly indicates that a particular condition or attribute is present
+false negative (FN)
+A test result which wrongly indicates that a particular condition or attribute is absent
