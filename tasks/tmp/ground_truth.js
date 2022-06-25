957fc621793d9fe6104c1d73002a5ab26d7af390
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-06-25 13:09:13 -0700

    Update ground_truth.js

--- tasks/tmp/ground_truth.js
+++ tasks/tmp/ground_truth.js
@@ -1,3 +1,8 @@
+"what": [
+      "The correct answer. Reality. Since reality is often subjective, expert raters typically are the proxy for ground truth.",
+      "[英] [related] [The correct answer]. [Reality]. [Since reality is often subjective], [expert raters] [typically are the proxy for ground truth].",
+      "[和] [関連] [正解]. [現実]. [現実はしばしば主観的であるため], [専門家の評価者][通常はグラウンドトゥルースの代理人です].",	    
+      "[漢] [相關] [正確答案]. [現實]. [由於現實通常是主觀的], [專家評估者] [通常是基本事實的代表].",
+    ],
 
 
-The correct answer. Reality. Since reality is often subjective, expert raters typically are the proxy for ground truth.
