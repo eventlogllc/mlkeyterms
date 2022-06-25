d406aa338cd3ded786dccc0663d48325ca9d3350
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-06-25 10:41:25 -0700

    Update decision_tree.js

--- tasks/tmp/decision_tree.js
+++ tasks/tmp/decision_tree.js
@@ -1,6 +1,6 @@
 "what": [
       "A supervised learning model composed of a set of conditions and leaves organized hierarchically.",
-      "[英] [related] The Bellman Equation, follows a specific policy and function, to give you, the best value, based on the states, it calculated recursively through dividing the states/paths up into subproblems or smaller problems. ",
-      "[和] [関連] [ベルマン方程式], [特定のポリシーと機能に従う], [与える], [最良の値], [状態に基づく], [再帰的に計算する], [状態/パスをサブ問題に分割することにより または小さな問題]",	    
-      "[漢] [相關] [貝爾曼方程], [遵循特定的策略和功能], [給你], [最佳價值], [基於狀態], [遞歸計算], [通過將狀態/路徑劃分為子問題 或更小的問題]",
+      "[英] [related] [A supervised learning model] [composed of a set of conditions] [and leaves organized hierarchically]",
+      "[和] [関連] [教師あり学習モデル] [一連の条件で構成] [階層的に編成された葉]",	    
+      "[漢] [相關] [一個有監督的學習模型] [由一組條件組成] [並且葉子分層組織]",
     ],
