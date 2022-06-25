99e2b0d4c14e7084b70dc3d7b9f05b81d7937070
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-06-25 13:05:23 -0700

    Update ROC.js

--- tasks/tmp/ROC.js
+++ tasks/tmp/ROC.js
@@ -1,4 +1,11 @@
+"what": [
+      "ROC (receiver operating characteristic) Curve",
+      "A curve of true positive rate vs. false positive rate at different classification thresholds. See also AUC.",
+      "[英] [related] [ROC (receiver operating characteristic) Curve]"
+      "[A curve of true positive rate] [ vs. false positive rate] [at different classification thresholds]. See also AUC.",
+      "[和] [関連] [ROC（受信者動作特性）曲線]"
+      "[真陽性率の曲線][対偽陽性率][異なる分類しきい値で]。 AUCも参照してください。",	    
+      "[漢] [相關] [ROC（接收器工作特性）曲線]",
+      "[真陽性率曲線] [與假陽性率] [在不同分類閾值下]。 另見 AUC。",
+    ],
 
-
-ROC (receiver operating characteristic) Curve
-A curve of true positive rate vs. false positive rate at different classification thresholds. See also AUC.
