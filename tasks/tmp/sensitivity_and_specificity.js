bfb800708ef2eab72b4456742727c11f21875886
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-06-25 11:01:06 -0700

    Update sensitivity_and_specificity.js

--- tasks/tmp/sensitivity_and_specificity.js
+++ tasks/tmp/sensitivity_and_specificity.js
@@ -1,10 +1,13 @@
-Sensitivity and specificity mathematically describe the accuracy of a test which reports the presence or absence of a condition. Individuals for which the condition is satisfied are considered "positive" and those for which it is not are considered "negative".
-
-Sensitivity (true positive rate) refers to the probability of a positive test, conditioned on truly being positive.
-Specificity (true negative rate) refers to the probability of a negative test, conditioned on truly being negative.
-
-True positive: Sick people correctly identified as sick
-False positive: Healthy people incorrectly identified as sick
-True negative: Healthy people correctly identified as healthy
-False negative: Sick people incorrectly identified as healthy
-
+"what": [
+      "Sensitivity and specificity mathematically describe the accuracy of a test which reports the presence or absence of a condition. Individuals for which the condition is satisfied are considered "positive" and those for which it is not are considered "negative".
+      "Sensitivity (true positive rate) refers to the probability of a positive test, conditioned on truly being positive"
+      "Specificity (true negative rate) refers to the probability of a negative test, conditioned on truly being negative"
+      "Application to screening study. Imagine a study evaluating a test that screens people for a disease",
+      "True positive: Sick people correctly identified as sick",
+      "False positive: Healthy people incorrectly identified as sick",
+      "True negative: Healthy people correctly identified as healthy",
+      "False negative: Sick people incorrectly identified as healthy",
+      "[英] [related] The Bellman Equation, follows a specific policy and function, to give you, the best value, based on the states, it calculated recursively through dividing the states/paths up into subproblems or smaller problems. ",
+      "[和] [関連] [ベルマン方程式], [特定のポリシーと機能に従う], [与える], [最良の値], [状態に基づく], [再帰的に計算する], [状態/パスをサブ問題に分割することにより または小さな問題]",	    
+      "[漢] [相關] [貝爾曼方程], [遵循特定的策略和功能], [給你], [最佳價值], [基於狀態], [遞歸計算], [通過將狀態/路徑劃分為子問題 或更小的問題]",
+    ],
