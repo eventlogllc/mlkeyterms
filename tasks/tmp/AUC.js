22c9180289d0a1d45c5e8bf32a88fd4a75bada23
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-06-25 12:53:58 -0700

    Update AUC.js

--- tasks/tmp/AUC.js
+++ tasks/tmp/AUC.js
@@ -1,6 +1,8 @@
+"what": [
+      "AUC (Area under the ROC Curve), An evaluation metric that considers all possible classification thresholds.",
+      "The Area Under the ROC curve is the probability that a classifier will be more confident that a randomly chosen positive example is actually positive than that a randomly chosen negative example is positive.",
+      "[英] [related] [AUC (Area under the ROC Curve)] [An evaluation metric that considers all possible classification thresholds]. [The Area Under the ROC curve] [is the probability that a classifier will be more confident] [that a randomly chosen positive example is actually positive] [than that a randomly chosen negative example is positive].",
+      "[和] [関連] [AUC（ROC曲線の下の領域）][すべての可能な分類しきい値を考慮した評価メトリック]。 [ROC曲線の下の領域][分類子がより自信を持っている確率です][ランダムに選択されたポジティブな例は実際にはポジティブな例です][ランダムに選択されたネガティブな例はポジティブな例ではありません].",	    
+      "[漢] [相關] [AUC（ROC 曲線下的面積）] [一個考慮所有可能分類閾值的評估指標]。 [ROC 曲線下的面積] [是分類器更有信心的概率] [隨機選擇的正例實際上是正例] [而不是隨機選擇的負例是正例].",
+    ],
 
-
-AUC (Area under the ROC Curve)
-An evaluation metric that considers all possible classification thresholds.
-
-The Area Under the ROC curve is the probability that a classifier will be more confident that a randomly chosen positive example is actually positive than that a randomly chosen negative example is positive.
