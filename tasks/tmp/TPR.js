ba351b32f7fbb69be16253f13954e76582abd79c
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-06-25 10:47:52 -0700

    Update TPR.js

--- tasks/tmp/TPR.js
+++ tasks/tmp/TPR.js
@@ -1,4 +1,9 @@
+"what": [
+      "true positive rate (TPR). Synonym for recall. True positive rate is the y-axis in an ROC curve. TPR =TP / (TP + FN} )",
+      "[英] [related] [true positive rate (TPR)] [Synonym for recall] [True positive rate] [is the y-axis] [in an ROC curve] [TPR] [=] [TP] [/] (TP + FN) ",
+      "[和] [関連] [真陽性率（TPR）][再現率の同義語][真陽性率][y軸][ROC曲線内][TPR] [=] [TP] [/]（TP + FN）",	    
+      "[漢] [相關] [真陽性率 (TPR)] [召回的同義詞] [真陽性率] [是 y 軸] [在 ROC 曲線中] [TPR] [=] [TP] [/] (TP + FN)",
+    ],
 
 
 
-true positive rate (TPR). Synonym for recall. True positive rate is the y-axis in an ROC curve. TPR =TP / (TP + FN} )
