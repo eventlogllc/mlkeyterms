11b3c100aaf33ba409697256a22c0969f1cef7f5
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-06-25 11:35:43 -0700

    Update true_false_positive_negative.js

--- tasks/tmp/true_false_positive_negative.js
+++ tasks/tmp/true_false_positive_negative.js
@@ -1,14 +1,27 @@
 
-
-condition positive (P)
-the number of real positive cases in the data
-condition negative (N)
-the number of real negative cases in the data
-true positive (TP)
-A test result that correctly indicates the presence of a condition or characteristic
-true negative (TN)
-A test result that correctly indicates the absence of a condition or characteristic
-false positive (FP)
-A test result which wrongly indicates that a particular condition or attribute is present
-false negative (FN)
-A test result which wrongly indicates that a particular condition or attribute is absent
+"what": [
+      "condition positive (P), the number of real positive cases in the data",
+      "condition negative (N), the number of real negative cases in the data",
+      "true positive (TP), "A test result that correctly indicates the presence of a condition or characteristic",
+      "true negative (TN), A test result that correctly indicates the absence of a condition or characteristic",
+      "false positive (FP), A test result which wrongly indicates that a particular condition or attribute is present",
+      "false negative (FN), A test result which wrongly indicates that a particular condition or attribute is absent",
+      "[英] [related] t[condition positive (P)] [the number of real positive cases] [in the data]",
+      "[condition negative (N)] [the number of real negative cases] [in the data]",
+      "[true positive (TP)] [A test result] [that correctly indicates] [the presence of a condition or characteristic]",
+      "[true negative (TN)] [A test result] [that correctly indicates] [the absence of a condition or characteristic]",
+      "[false positive (FP)] [A test result] [which wrongly indicates] [that a particular condition or attribute is present]",
+      "[false negative (FN)] [A test result] [which wrongly indicates] [that a particular condition or attribute is absent]",
+      "[和] [関連] [条件陽性（P）][実際の陽性症例数][データ内]",
+      "[条件陰性（N）][実際の陰性症例数][データ内]",
+      "[真陽性（TP）][テスト結果][正しく示す][状態または特性の存在]",
+      "[真陰性（TN）][テスト結果][正しく示す][状態または特性がない]",
+      "[偽陽性（FP）][テスト結果][誤って示す][特定の条件または属性が存在すること]",
+      "[偽陰性（FN）][テスト結果][誤って示す][特定の条件または属性が存在しないこと]",   
+      "[漢] [相關] [條件陽性（P）] [真實陽性病例數] [數據中]",
+      "[條件否定（N）] [真實否定案例的數量] [數據中]",
+      "[真陽性 (TP)] [測試結果] [正確表明] [存在某種狀況或特徵]",
+      "[真陰性 (TN)] [測試結果] [正確指示] [沒有條件或特徵]",
+      "[誤報 (FP)] [測試結果] [錯誤地表明] [存在特定條件或屬性]",
+      "[假陰性 (FN)] [測試結果] [錯誤地表明] [特定條件或屬性不存在]",
+    ],
