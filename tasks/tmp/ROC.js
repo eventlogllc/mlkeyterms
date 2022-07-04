b4e1a251805fe9b9f076d564428e42a27681297f
akiko-113 <104332806+akiko-113@users.noreply.github.com>
2022-07-04 21:13:53 +0900

    Update ROC.js

--- tasks/tmp/ROC.js
+++ tasks/tmp/ROC.js
@@ -9,3 +9,10 @@
       "[真陽性率曲線] [與假陽性率] [在不同分類閾值下]。 另見 AUC。",
     ],
 
+"[英] [related][和] [関連][漢] [相關]
+[ROC (receiver operating characteristic) Curve][ROC（受信者動作特性）曲線] [ROC（接收器工作特性）曲線]
+[A curve of true positive rate] [真陽性率の曲線][真陽性率曲線]
+[ vs. false positive rate] [対偽陽性率][與假陽性率] 
+[at different classification thresholds][異なる分類しきい値で][在不同分類閾值下]
+See also AUC.[AUCも参照してください][另見 AUC]
+     
