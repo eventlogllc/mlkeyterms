78430a423bd8c8b7fc7dc3f37f788de48d4449c6
akiko-113 <104332806+akiko-113@users.noreply.github.com>
2022-07-04 20:56:29 +0900

    Update AUC.js

--- tasks/tmp/AUC.js
+++ tasks/tmp/AUC.js
@@ -6,3 +6,11 @@
       "[漢] [相關] [AUC（ROC 曲線下的面積）] [一個考慮所有可能分類閾值的評估指標]。 [ROC 曲線下的面積] [是分類器更有信心的概率] [隨機選擇的正例實際上是正例] [而不是隨機選擇的負例是正例].",
     ],
 
+"[英] [related]"[和] [関連]"[漢] [相關] 
+[AUC (Area under the ROC Curve)][AUC（ROC曲線の下の領域）] [AUC（ROC 曲線下的面積）] 
+[An evaluation metric that considers all possible classification thresholds] [すべての可能な分類しきい値を考慮した評価メトリック][一個考慮所有可能分類閾值的評估指標]
+[The Area Under the ROC curve] [ROC曲線の下の領域] [ROC 曲線下的面積]
+[is the probability that a classifier will be more confident] [分類子がより自信を持っている確率です] [是分類器更有信心的概率] 
+[that a randomly chosen positive example is actually positive] [ランダムに選択されたポジティブな例は実際にはポジティブな例です][隨機選擇的正例實際上是正例]
+[than that a randomly chosen negative example is positive][ランダムに選択されたネガティブな例はポジティブな例ではありません] [而不是隨機選擇的負例是正例]
+     
