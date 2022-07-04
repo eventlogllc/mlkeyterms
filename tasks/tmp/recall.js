1b96c4c9f761a39d039a88fd9bb6fbd6a30f0608
akiko-113 <104332806+akiko-113@users.noreply.github.com>
2022-07-04 22:52:26 +0900

    Update recall.js

--- tasks/tmp/recall.js
+++ tasks/tmp/recall.js
@@ -11,15 +11,15 @@ recall
 A metric for classification models that answers the following question: Out of all the possible positive labels, how many did the model correctly identify? That is:
 
 [英] [related] [和] [関連][漢] [相關] 
-[a] [一種の][一種]
-[metric for classification models] [分類モデルの測定基準] [分類模型的指標]
-[that answers the following question] [次の質問に答える][回答以下問題]
-[Out of all the possible positive labels] [考えられるすべての陽性ラベルのうち][在所有可能的正面標籤中]
-[how many did the model correctly identify?] [モデルが正しく識別した数] [模型正確識別了多少？]
-[recall]  [再現率][回憶] 
+[a] [一種の][isshu no][いっしゅの][一種]
+[metric for classification models] [分類モデルの測定基準] [bunrui moderu no sokutei kijun][ぶんるいモデルのそくていきじゅん][分類模型的指標]
+[that answers the following question] [次の質問に答える][tsugi no shitsumon ni kotaeru][つぎのしつもんにこたえる][回答以下問題]
+[Out of all the possible positive labels] [考えられるすべての陽性ラベルのうち][kangaerareru subete no yousei raberu no uchi][かんがえられるすべてのようせいラベルのうち][在所有可能的正面標籤中]
+[how many did the model correctly identify?] [いくつモデルが正しく識別したか？][ikutsu moderu ga tadashiku shikibetsu shitaka?][いくつモデルがただしくしきべつしたか] [模型正確識別了多少？]
+[recall]  [再現率][saigenritsu][さいげんりつ][回憶] 
 [=] [=] [=] 
- [true-positive] [真陽性][真陽性]
+ [true-positive] [真陽性][shin yousei][しんようせい][真陽性]
 [/]  [/ ][/ ]
- [( true-positive + false-negative )][（true-positive + false-negative）][( 真陽性 + 假陰性 )]
+ [( true-positive + false-negative )][（真陽性 + 偽陰性）][(shin yousei+gi insei)][(しんようせい+ぎいんせい)][( 真陽性 + 假陰性 )]
  
      
