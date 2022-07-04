d2f184d0662b9a2db91d7556539e67ab6dfbdbf4
akiko-113 <104332806+akiko-113@users.noreply.github.com>
2022-07-04 21:03:20 +0900

    Update FPR.js

--- tasks/tmp/FPR.js
+++ tasks/tmp/FPR.js
@@ -15,3 +15,14 @@
        "[其中 FP 是假陽性的數量], [TN 是真陰性的數量], [並且 FP + TN 是地面真陰性的總數].",
     ],
 
+"[英] [related] [和] [関連[漢] [相關] 
+[false positive rate (FPR)],[偽陽性率（FPR）][假陽性率 (FPR)]
+      [The x-axis in an ROC curve][ROC曲線のx軸][ROC 曲線中的 x 軸]
+      [The false positive rate] [偽陽性率]【誤報率】
+      [is defined as follows][次のように定義されます]【定義如下
+      [The false positive rate] [偽陽性率][誤報率]
+      [is FPR = FP / ( FP  + TN ) ][はFPR=FP /（FP + TN）][是 FPR = FP / ( FP + TN ) ]
+      "[where FP  is the number of false positives][ここで、FPは誤検出の数][其中 FP 是假陽性的數量]
+      [TN is the number of true negatives][TNは真の負の数] [TN 是真陰性的數量]
+      "[and FP + TN is the total number of ground truth negatives][FP +TNはグラウンドトゥルースの負の総数です][並且 FP + TN 是地面真陰性的總數]
+     
