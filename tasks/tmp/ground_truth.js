370c39fae66e60bb110bf10056867c0ef6983b99
akiko-113 <104332806+akiko-113@users.noreply.github.com>
2022-07-04 22:47:02 +0900

    Update ground_truth.js

--- tasks/tmp/ground_truth.js
+++ tasks/tmp/ground_truth.js
@@ -7,9 +7,9 @@
 
 
 [英] [related] [和] [関連] [漢] [相關]
-[The correct answer][正解][正確答案]
-[Reality][現実][現實]
-[Since reality is often subjective][現実はしばしば主観的であるため][由於現實通常是主觀的]
-[expert raters] [専門家の評価者][專家評估者]
-[typically are the proxy for ground truth][通常はグラウンドトゥルースの代理人です][通常是基本事實的代表]
+[The correct answer][正解][seikai][せいかい][正確答案]
+[Reality][現実][genjitsu][げんじつ][現實]
+[Since reality is often subjective][現実はしばしば主観的であるため][genjitsu wa shibashiba shukanteki de arutame][げんじつはしばしばしゅかんてきであるため][由於現實通常是主觀的]
+[expert raters] [専門家の評価者][senmonka no hyoukashya][せんもんかのひょうかしゃ][專家評估者]
+[typically are the proxy for ground truth][通常はグラウンドトゥルースのプロキシである][tsuujou wa gurando truth no purokishi dearu][つうじょうはグラウンドトゥルースのプロキシである][通常是基本事實的代表]
      
