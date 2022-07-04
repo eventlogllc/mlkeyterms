dfb2f8be95856585e8a1f3319a8165d89f415e29
akiko-113 <104332806+akiko-113@users.noreply.github.com>
2022-07-04 21:49:35 +0900

    Update true_false_positive_negative.js

--- tasks/tmp/true_false_positive_negative.js
+++ tasks/tmp/true_false_positive_negative.js
@@ -25,3 +25,27 @@
       "[誤報 (FP)] [測試結果] [錯誤地表明] [存在特定條件或屬性]",
       "[假陰性 (FN)] [測試結果] [錯誤地表明] [特定條件或屬性不存在]",
     ],
+"[英] [related] [和] [関連][漢] [相關]
+[condition positive (P)] [条件陽性（P）] [條件陽性（P）]
+[the number of real positive cases] [実際の陽性症例数][真實陽性病例數]
+[in the data][データ内][數據中]
+[condition negative (N)] [条件陰性（N）][條件否定（N）] 
+[the number of real negative cases] [実際の陰性症例数][真實否定案例的數量]
+[in the data][データ内][數據中]
+[true positive (TP)] [真陽性（TP）][真陽性 (TP)]
+[A test result] [テスト結果][測試結果]
+[that correctly indicates] [正しく示す][正確表明]
+[the presence of a condition or characteristic][状態または特性の存在][存在某種狀況或特徵]
+[true negative (TN)] [真陰性（TN）][真陰性 (TN)]
+[A test result] [テスト結果][測試結果]
+[that correctly indicates] [正しく示す][正確指示]
+[the absence of a condition or characteristic][状態または特性がない] [沒有條件或特徵]
+[false positive (FP)] [偽陽性（FP）][誤報 (FP)] 
+[A test result] [テスト結果][測試結果]
+[which wrongly indicates] [誤って示す] [錯誤地表明] 
+[that a particular condition or attribute is present][特定の条件または属性が存在すること][存在特定條件或屬性]
+[false negative (FN)] [偽陰性（FN）][假陰性 (FN)] 
+[A test result] [テスト結果][測試結果]
+[which wrongly indicates] [誤って示す][錯誤地表明] 
+[that a particular condition or attribute is absent][特定の条件または属性が存在しないこと][特定條件或屬性不存在]
+     
