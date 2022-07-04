4c07fc21e3c186702765fbce8a3a779185d2b0e6
akiko-113 <104332806+akiko-113@users.noreply.github.com>
2022-07-04 22:36:45 +0900

    Update decision_tree.js

--- tasks/tmp/decision_tree.js
+++ tasks/tmp/decision_tree.js
@@ -5,7 +5,7 @@
       "[漢] [相關] [一個有監督的學習模型] [由一組條件組成] [並且葉子分層組織]",
     ],
 [英] [related] [和] [関連] [漢] [相關]
-[A supervised learning model][教師あり学習モデル] [一個有監督的學習模型]
-[composed of a set of conditions] [一連の条件で構成][由一組條件組成] 
-[and leaves organized hierarchically] [階層的に編成された葉][並且葉子分層組織]
+[A supervised learning model][教師あり学習モデル] [kyoushi ari gakushu moderu][きょうしありがくしゅうモデル][一個有監督的學習模型]
+[composed of a set of conditions] [一連の条件で構成された][ichiren no jouken de kousei sareta][いちれんのじょうけんでこうせいされた][由一組條件組成] 
+[and leaves organized hierarchically] [階層的に編成された葉][kaisouteki ni hennsei sareta ha][かいそうてきにへんせいされたは][並且葉子分層組織]
     
