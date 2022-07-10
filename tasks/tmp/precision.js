8714c1e2e0b7e57097a8f86f5a308f833b861a47
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-07-09 18:17:21 -0700

    Update precision.js

--- tasks/tmp/precision.js
+++ tasks/tmp/precision.js
@@ -1,10 +1,8 @@
 "what": [
       "A metric for classification models. Precision identifies the frequency with which a model was correct when predicting the positive class.",
       "precision = true_positives / ( true_positives + false_positives )",
-      "[英] [related] The Bellman Equation, follows a specific policy and function, to give you, the best value, based on the states, it calculated recursively through dividing the states/paths up into subproblems or smaller problems. ",
-      "[和] [関連] [一種] [分類モデルのメトリック][精度はモデルが正しい頻度を識別します][ポジティブクラスを予測する場合][数学][精度][=][真陽性][/][真- ポジティブ+フォールス-ポジティブ]",	    
-      "[漢] [相關] [一種] [分類模型的指標] [精度識別模型正確的頻率] [在預測正類時] [精度] [=] [真陽性] [/] [真陽性 + 假陽性]",
+      "[英] [related]  [A metric] [for classification models]. [Precision identifies the frequency with which a model was correct] [when predicting the positive class] [precision] [=] [true_positives] [/] [ true_positives] + [false_positives]",
+      "[和] [関連] [一種] [分類モデルのメトリック][精度はモデルが正しい頻度を識別します][ポジティブクラスを予測する場合][数学][精度][=][真陽性][/]（[真陽性] + [偽陽性]）,	    
+      "[漢] [相關] [一種] [分類模型的指標] [精度識別模型正確的頻率] [在預測正類時] [精度] [=] ([真陽性] [/] [真陽性 + 假陽性])",
     ],
 
-precision
-A metric for classification models. Precision identifies the frequency with which a model was correct when predicting the positive class. 
