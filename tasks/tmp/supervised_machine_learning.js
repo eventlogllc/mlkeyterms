fa9875564ae5a16da47a63542baac41277220045
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-07-10 11:00:15 -0700

    Update supervised_machine_learning.js

--- tasks/tmp/supervised_machine_learning.js
+++ tasks/tmp/supervised_machine_learning.js
@@ -5,8 +5,8 @@
       "After mastering the mapping between questions and answers,", 
       "the student can then provide answers to new (never-before-seen) questions on the same topic.",
       "Compare with unsupervised machine learning.",
-      "[英] [related]  [A metric] [for classification models]. [Precision identifies the frequency with which a model was correct] [when predicting the positive class] [precision] [=] [true_positives] [/] [ true_positives] + [false_positives]",
-      "[和] [関連] [一種] [分類モデルのメトリック][精度はモデルが正しい頻度を識別します][ポジティブクラスを予測する場合][数学][精度][=][真陽性][/]（[真陽性] + [偽陽性]）,	    
-      "[漢] [相關] [一種] [分類模型的指標] [精度識別模型正確的頻率] [在預測正類時] [精度] [=] ([真陽性] [/] [真陽性 + 假陽性])",  
+      "[英] [related]  [Training a model] [from input data] [and its corresponding labels.]",
+      "[和] [関連] [一種] [トレーニングモデル][入力データから][および対応するラベル。],	    
+      "[漢] [相關] [一種] [訓練模型] [來自輸入數據] [及其相應的標籤.]",  
     ],",
 
