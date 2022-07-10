5fb308d8eca398e107bcf3df8d999c5eb3d958b5
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-07-10 08:21:16 -0700

    Create supervised_training.js

---
+++ tasks/tmp/supervised_training.js
@@ -0,0 +1,3 @@
+    "what": [
+      "Training a model from input data and its corresponding labels. Supervised machine learning is analogous to a student learning a subject by studying a set of questions and their corresponding answers.  After mastering the mapping between questions and answers, the student can then provide answers to new (never-before-seen) questions on the same topic.  Compare with unsupervised machine learning."
+    ],
