1ee39ac23fd37d7ebd6dee04c063140a33755a37
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-07-10 11:52:37 -0700

    Update weight.js

--- tasks/tmp/weight.js
+++ tasks/tmp/weight.js
@@ -1,2 +1,13 @@
 "what": [
-      "A coefficient for a feature in a linear model, or an edge in a deep network. The goal of training a linear model is to determine the ideal weight for each feature. If a weight is 0, then its corresponding feature does not contribute to the model."
+      "A coefficient for a feature in a linear model,or an edge in a deep network.", 
+      "The goal of training a linear model is to determine the ideal weight for each feature.",
+      "If a weight is 0, then its corresponding feature does not contribute to the model.",
+      "[英] [related]  [A coefficient for a feature in a linear model, ] [or an edge in a deep network.]",
+      "[The goal of training a linear model] [is to determine the ideal weight for each feature.],"
+      "[If a weight is 0], [then its corresponding feature] [does not contribute to the model.]",
+      "[和] [関連] [線形モデルの特徴の係数][または深いネットワークのエッジ。]",
+      "[線形モデルのトレーニングの目標][各特徴の理想的な重みを決定することです.]",
+      "[重みが0の場合,] [対応する機能][モデルに寄与しません.]" 
+      "[漢] [相關] [線性模型中特徵的係數,] [或深度網絡中的邊緣]",
+      "[訓練線性模型的目標] [是確定每個特徵的理想權重.]",
+      "[如果權重為 0], [那麼其對應的特徵] [對模型沒有貢獻. ]"
