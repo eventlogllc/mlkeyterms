c4257fbf1d57766ae5ccf8219e830084dc81ba76
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-07-10 11:47:25 -0700

    Update validation.js

--- tasks/tmp/validation.js
+++ tasks/tmp/validation.js
@@ -1,4 +1,10 @@
     "what": [
-      "A process used, as part of training, to evaluate the quality of a machine learning model using the validation set. Because the validation set is disjoint from the training set, validation helps ensure that the model\u2019s performance generalizes beyond the training set.",
+      "A process used, as part of training,",
+      "to evaluate the quality of a machine learning model using the validation set.",
+      "Because the validation set is disjoint from the training set,",
+      "validation helps ensure that the model\u2019s performance generalizes beyond the training set.",
       "Contrast with test set."
+      "[英] [related]  [A process used as part of training] [to evaluate the quality of a machine learning model] [using the validation set.]",
+      "[和] [関連] [トレーニングの一部として使用されるプロセス][機械学習モデルの品質を評価するため][検証セットを使用して]",
+      "[漢] [相關] [用作訓練一部分的過程] [評估機器學習模型的質量] [使用驗證集。]", 
     ],
