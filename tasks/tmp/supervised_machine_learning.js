b6eb34cc1145ca73a63693548bcb183251065a62
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-07-10 10:47:33 -0700

    Update supervised_machine_learning.js

--- tasks/tmp/supervised_machine_learning.js
+++ tasks/tmp/supervised_machine_learning.js
@@ -1,3 +1,12 @@
     "what": [
-      "Training a model from input data and its corresponding labels. Supervised machine learning is analogous to a student learning a subject by studying a set of questions and their corresponding answers.  After mastering the mapping between questions and answers, the student can then provide answers to new (never-before-seen) questions on the same topic.  Compare with unsupervised machine learning."
-    ],
+      "Training a model from input data and its corresponding labels.",
+      "Supervised machine learning is analogous to a student learning a subject",
+      "by studying a set of questions and their corresponding answers.",
+      "After mastering the mapping between questions and answers,", 
+      "the student can then provide answers to new (never-before-seen) questions on the same topic.",
+      "Compare with unsupervised machine learning.",
+      "[英] [related]  [A metric] [for classification models]. [Precision identifies the frequency with which a model was correct] [when predicting the positive class] [precision] [=] [true_positives] [/] [ true_positives] + [false_positives]",
+      "[和] [関連] [一種] [分類モデルのメトリック][精度はモデルが正しい頻度を識別します][ポジティブクラスを予測する場合][数学][精度][=][真陽性][/]（[真陽性] + [偽陽性]）,	    
+      "[漢] [相關] [一種] [分類模型的指標] [精度識別模型正確的頻率] [在預測正類時] [精度] [=] ([真陽性] [/] [真陽性 + 假陽性])",  
+    ],",
+
