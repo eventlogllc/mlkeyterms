aef29f10aa795210403d9d492392ec77806eacc6
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-07-09 18:09:39 -0700

    Update rater.js

--- tasks/tmp/rater.js
+++ tasks/tmp/rater.js
@@ -1,3 +1,8 @@
+"what": [
+      "A human who provides labels in examples. Sometimes called an 'annotator.'",
+      "The Area Under the ROC curve is the probability that a classifier will be more confident that a randomly chosen positive example is actually positive than that a randomly chosen negative example is positive.",
+      "[英] [A human] [who provides labels ] [in examples]. [Sometimes called] [an 'annotator']",
+      "[和] [関連] [一人の人間] [ラベルを提供します] [例では]. [時々呼ばれる]['アノテーター']",	    
+      "[漢] [一個人] [提供標籤] [在示例中]. [有時稱為] ['註釋者']",
+    ],
 
-
-A human who provides labels in examples. Sometimes called an "annotator."
