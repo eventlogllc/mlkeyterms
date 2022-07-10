e657dec6fccc004be386d64334fc0799befee048
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-07-10 08:19:49 -0700

    Create validation.js

---
+++ tasks/tmp/validation.js
@@ -0,0 +1,4 @@
+    "what": [
+      "A process used, as part of training, to evaluate the quality of a machine learning model using the validation set. Because the validation set is disjoint from the training set, validation helps ensure that the model\u2019s performance generalizes beyond the training set.",
+      "Contrast with test set."
+    ],
