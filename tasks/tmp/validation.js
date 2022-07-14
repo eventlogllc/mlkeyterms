b3b7c7516895811704f7611388321563329a3572
akiko-113 <104332806+akiko-113@users.noreply.github.com>
2022-07-14 22:05:38 +0900

    Update validation.js

--- tasks/tmp/validation.js
+++ tasks/tmp/validation.js
@@ -8,3 +8,7 @@
       "[和] [関連] [トレーニングの一部として使用されるプロセス][機械学習モデルの品質を評価するため][検証セットを使用して]",
       "[漢] [相關] [用作訓練一部分的過程] [評估機器學習模型的質量] [使用驗證集。]", 
     ],
+[英] [related] [和] [関連] [漢] [相關]
+    [A process used as part of training] [トレーニングの一部として使用されるプロセス][toreininngu no ichibu toshite shiyou sareru purosesu][トレーニングのいちぶとしてしようされるプロセス][用作訓練一部分的過程]
+    [to evaluate the quality of a machine learning model]  [機械学習モデルの品質を評価するため] [kikaigakushuu moderu no hinshitsu wo hyoukasuru tame][きかいがくしゅうモデルのひんしつをひょうかするため][評估機器學習模型的質量]
+    [using the validation set.][検証セットを使用して][kenshou setto wo shiyou shite][けんしょうセットをしようして][使用驗證集。]
