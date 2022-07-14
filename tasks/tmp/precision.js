7864e1bb554da2b89561c155cfd7fc14db38f108
akiko-113 <104332806+akiko-113@users.noreply.github.com>
2022-07-14 21:40:03 +0900

    Update precision.js

--- tasks/tmp/precision.js
+++ tasks/tmp/precision.js
@@ -6,3 +6,15 @@
       "[漢] [相關] [一種] [分類模型的指標] [精度識別模型正確的頻率] [在預測正類時] [精度] [=] ([真陽性] [/] [真陽性 + 假陽性])",
     ],
 
+[英] [related]  "[和] [関連]"[漢] [相關]
+★★[A metric]  [距離][kyori][きょり]  [一種] 
+★★[for classification models] [分類モデル][bunrui moderu][ぶんるいもでる][分類模型的指標]
+[Precision identifies the frequency with which a model was correct]  [精度はモデルが正しい頻度を識別する][seido wa moderu ga tadashii hindo wo shikibetsu suru][せいどはモデルがただしいせいどをしきべつする] [精度識別模型正確的頻率] 
+[when predicting the positive class]  [陽性クラスを予測する場合][yousei kurasu wo yosoku suru baai][ようせいクラスをよそくするばあい] [在預測正類時]
+[precision] [精度][seido][せいど][精度]
+[=] [=] [=] 
+ [true_positives][真陽性][shin yousei][しんようせい][真陽性]
+[/] [/] [/]
+ [ true_positives] + [false_positives]",（[真陽性] + [偽陽性]）([shin yousei]+[gi yousei])([しんようせい]+[ぎようせい])([真陽性 + 假陽性])
+  ★★ [数学],	    
+     ",
