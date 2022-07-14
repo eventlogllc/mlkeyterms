85f7e618c3d74fed7e91bd8ec80b81a8aca20b05
akiko-113 <104332806+akiko-113@users.noreply.github.com>
2022-07-14 22:18:03 +0900

    Update weight.js

--- tasks/tmp/weight.js
+++ tasks/tmp/weight.js
@@ -11,3 +11,12 @@
       "[漢] [相關] [線性模型中特徵的係數,] [或深度網絡中的邊緣]",
       "[訓練線性模型的目標] [是確定每個特徵的理想權重.]",
       "[如果權重為 0], [那麼其對應的特徵] [對模型沒有貢獻. ]"
+[英] [related]  [和] [関連] [漢] [相關] 
+[A coefficient for a feature in a linear model, ] [線形モデルの特徴の係数][senkei moderu no tokuchou no keisuu][せんけいモデルのとくちょうのけいすう][線性模型中特徵的係數,] 
+[or an edge in a deep network.][または、ディープネットワークのエッジ][matawa, deep netto waku no ejji][または、ディープネットワークのエッジ][或深度網絡中的邊緣]
+[The goal of training a linear model] [線形モデルのトレーニングの目標][senkei moderu no toreiningu no mokuhyou][せんけいモデルのトレーニングのもくひょう][訓練線性模型的目標]
+[is to determine the ideal weight for each feature.][各特徴の理想的な重みを決定することである。][kaku tokuchou no risouteki na omomi wo ketteisuru kotodearu][かくとくちょうのりそうてきなおもみをけっていすることである][是確定每個特徵的理想權重.]
+[If a weight is 0][重みが0の場合,][omomi ga zero no baai][おもみがぜろのばあい][如果權重為 0]
+[then its corresponding feature] [対応する機能][taiou suru kinou][たいおうするきのう][那麼其對應的特徵] 
+[does not contribute to the model.][モデルに寄与しない.][moderu ni kiyo shinai][モデルにきよしない][對模型沒有貢獻. ]
+     
