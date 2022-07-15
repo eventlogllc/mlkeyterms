af6570001f6d83811d806c90bb4f00d34e8f455c
akiko-113 <104332806+akiko-113@users.noreply.github.com>
2022-07-15 11:01:02 +0900

    Update Bellman_equation.js

--- tasks/Bellman_equation.js
+++ tasks/Bellman_equation.js
@@ -4,7 +4,7 @@
       "which it calculated recursively through dividing the states/paths up into subproblems or smaller problems. ",
       "For example, it might do 'State 0 to State 1 or State 2' and find out the optimal value and then it will find 'State 2 to State 3' and find optimal value.",
       "And then give you the most optimal value.",
-      "[英] [related] The Bellman Equation, follows a specific policy and function, to give you, the best value, based on the states, it calculated recursively through dividing the states/paths up into subproblems or smaller problems. ",
+      "[英] [related] The Bellman Equation, follows a specific policy and function, to give you, the best value, based on the states, it calculated recursively, through dividing the states/paths up into subproblems or smaller problems. ",
       "[和] [関連] [ベルマン方程式], [特定のポリシーと機能に従う], [与える], [最良の値], [状態に基づく], [再帰的に計算する], [状態/パスをサブ問題に分割することにより または小さな問題]",	    
       "[漢] [相關] [貝爾曼方程], [遵循特定的策略和功能], [給你], [最佳價值], [基於狀態], [遞歸計算], [通過將狀態/路徑劃分為子問題 或更小的問題]",
     ],
@@ -12,9 +12,8 @@
 [英] [related] "[和] [関連],[漢] [相關] 
 The Bellman Equation,  [ベルマン方程式][beruman houteishiki][ベルマンほうていしき],[貝爾曼方程]
 follows a specific policy and function, [特定のポリシーと機能に従う][tokutei no porishii to kinou ni shitagau][とくていのポリシーときのうにしたがう], [遵循特定的策略和功能]
-to give you, [与える],[ataeru][あたえる], [給你]
-the best value, [最良の値][sairyou no atai][さいりょうのあたい] , [最佳價值]
-based on the states, [状態に基づく] [joutai ni motoduku][じょうたいにもとづく], [基於狀態]
+to give you the best value, [最良の値を与えるため],[sairyou no atai wo ataeru tame][さいりょうのあたいをあたえるため], [給你最佳價值]
+based on the states, [状態に基づいて] [joutai ni motoduite][じょうたいにもとづいて], [基於狀態]
 it calculated recursively, [再帰的に計算する][saikiteki ni keisansuru] [さいきてきにけいさんする], [遞歸計算]
 through dividing the states/paths up into subproblems or smaller problems.,[状態/パスをサブ問題もしくはより小さな問題に分割することにより],[joutai/pasu wo sabu mondai moshikuwa yori chiisana mondai ni bunkatsusuru kotoniyori]
 [じょうたい/パスをサブもんだいもしくはよりちいさなもんだいにぶんかつすることにより] [通過將狀態/路徑劃分為子問題 或更小的問題]	    
