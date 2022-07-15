02fc14ff49889206ecb97d1fe2aba27795a7d544
akiko-113 <104332806+akiko-113@users.noreply.github.com>
2022-07-15 11:11:11 +0900

    Update FPR.js

--- tasks/FPR.js
+++ tasks/FPR.js
@@ -20,9 +20,9 @@
       [The x-axis in an ROC curve][ROC曲線のx軸][ROC kyokusen no X jiku][ROCきょくせんのXじく][ROC 曲線中的 x 軸]
       [The false positive rate] [偽陽性率][giyouseiritsu][ぎようせいりつ]【誤報率】
       [is defined as follows][次のように定義される][tsugi no youni teigi sareru][つぎのようにていぎされる]【定義如下
-      [The false positive rate] [偽陽性率][giyouseiritsu][ぎようせいりつ][誤報率]
-      [is FPR = FP / ( FP  + TN ) ][はFPR=FP /（FP + TN）][wa FPR=FP /（FP + TN）][はFPR=FP /（FP + TN）][是 FPR = FP / ( FP + TN ) ]
-      "[where FP  is the number of false positives][ここで、FPは誤検出の数][kokode FP wa gokenshutsu no kazu][ここでFPはごけんしゅつのかず][其中 FP 是假陽性的數量]
+      [The false positive rate is] [偽陽性率は][giyouseiritsu wa][ぎようせいりつは][誤報率是]
+      [FPR = FP / ( FP  + TN ) ][FPR=FP /（FP + TN）][FPR=FP /（FP + TN）][FPR=FP /（FP + TN）][FPR = FP / ( FP + TN ) ]
+      "[FP  is the number of false positives][FPは誤検出の数][FP wa gokenshutsu no kazu][FPはごけんしゅつのかず][其中 FP 是假陽性的數量]
       [TN is the number of true negatives][TNは真の負の数] [TN wa shin no fu no kazu][TNはしんのふのかず][TN 是真陰性的數量]
       "[and FP + TN is the total number of ground truth negatives][FP +TNはグラウンドトゥルースの負の総数である][FP+TN wa gurando truth no fu no sousuu dearu][FP+TNはグラウンドトゥルースのふのそうすうである][並且 FP + TN 是地面真陰性的總數]
      
