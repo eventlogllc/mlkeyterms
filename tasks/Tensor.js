7bf35acc51bcbe3df1d85d848fa0c2b553492528
akiko-113 <104332806+akiko-113@users.noreply.github.com>
2022-07-19 22:26:21 +0900

    Update Tensor.js

--- tasks/Tensor.js
+++ tasks/Tensor.js
@@ -11,8 +11,9 @@
 [英] [related][和] [関連][kanren][かんれん][漢] [相關]
 [In mathematics] [数学では][suugaku dewa][すうがくでは] [在數學中] 
 [a tensor is an algebraic object] [テンソルは代数的オブジェクトである][tensoru wa daisuu teki obujekuto dearu][張量] [是] [一個] [代數對象] 
-[ describes a multilinear relationship between sets of algebraic objects related to a vector space]
+★★★[ describes a multilinear relationship between sets of algebraic objects related to a vector space]
 [ベクトル空間に関連する代数的オブジェクトの集合の間の多重線形の関係を記述している][bekutoru kuukann ni kanrensuru daisuu teki obujekuto no shuugou no aida no dajuusenkei no kanikei wo kijutsu shiteiru]                                         kannkei wo
                                        
 [描述了與向量空間相關的代數對象集合之間的多線性關係]
 
+★★★the sentence seems to be long..?
