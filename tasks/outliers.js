452f77fdb461e68ad37081cfe686e71817278f87
akiko-113 <104332806+akiko-113@users.noreply.github.com>
2022-07-19 22:39:18 +0900

    Update outliers.js

--- tasks/outliers.js
+++ tasks/outliers.js
@@ -10,15 +10,15 @@
     ],
 
         "[英] [related] "[和] [関連]"[漢] [相關]
-    [Values distant from most other values] [他のほとんどの値から離れた値][與大多數其他值相距甚遠的值]
-    [In machine learning] [機械学習の場合][在機器學習中] 
-    [any of the following are outliers][次のいずれかが異常値です] [以下任何一項都是異常值] 
-    [with high absolute values] [絶対値が高い][具有高絕對值]
-    [Predicted values relatively far away from the actual values] [実際の値から比較的離れた予測値] [預測值與實際值相距較遠]
+    [Values distant from most other values] [他のほとんどの値から離れた値][hokano hotondo no atai kara hanareta atai][ほかのほとんどのあたいからはなれたあたい][與大多數其他值相距甚遠的值]
+    [In machine learning] [機械学習の場合][kikai gakushuu no baai][きかいがくしゅうのばあい][在機器學習中] 
+    [any of the following are outliers][次のいずれかが異常値である][tsugi no izureka ga ijouchi dearu][つぎのいずれかがいじょうちである] [以下任何一項都是異常值] 
+    [with high absolute values] [絶対値が高い][zettai chi ga takai][ぜったいちがｔかい][具有高絕對值]
+    [Predicted values relatively far away from the actual values] [実際の値から比較的離れた予測値] [jissai no atai kara hikakuteki hanareta yosokuchi][じっさいのあたいからひかくてきはなれたよそくち][預測值與實際值相距較遠]
     [Input data whose values are more than roughly 3 standard deviations from the mean],[ 平均から値が約3標準偏差を超える入力データ]
         [heikin kara atai ga yaku 3 hyoujunn hennsa wo koeru nyuuryoku deta][へいきんからあたいがやく３ひょうじゅんへんさをこえるにゅうりょくデータ][輸入數據的值大於大約 3 個標準差 從平均數]",
-    [Outliers often cause problems in model training][異常値はモデルトレーニングでよく問題を起こす][ijouchi wa moderu treiningu de yoku mondai wo okosu]
-[Clipping is one way of managing outliers][クリッピングは異常値を管理するひとつの方法だ][kurippingu wa ijouchi wo kanrisuru hitotsu no houhou da]
+    [Outliers often cause problems in model training][異常値はモデルトレーニングでよく問題を起こす][ijouchi wa moderu treiningu de yoku mondai wo okosu][いじょうちはモデルトレーニングでよくもんだいをおこす]
+[Clipping is one way of managing outliers][クリッピングは異常値を管理するひとつの方法だ][kurippingu wa ijouchi wo kanrisuru hitotsu no houhou da][クリッピングはいじょうちをかんりするひとつのほうほうだ]
 
         
     ],
