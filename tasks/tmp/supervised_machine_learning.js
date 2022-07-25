34657d3d789280edb4ae01b2445f2b71bd5d3cdf
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-07-24 21:41:49 -0700

    Update supervised_machine_learning.js

--- tasks/tmp/supervised_machine_learning.js
+++ tasks/tmp/supervised_machine_learning.js
@@ -5,6 +5,8 @@
       "After mastering the mapping between questions and answers,", 
       "the student can then provide answers to new (never-before-seen) questions on the same topic.",
       "Compare with unsupervised machine learning.",
+      "[和] 教師あり機械学習は, 入力データとそれに対応するラベルからモデルをトレーニングすることです. 教師あり機械学習は, 一連の質問とそれに対応する回答を学習することで, 学生が主題を学習するのに似ています.",
+      "[漢] 監 督 機 器 學 習 是 從 輸 入 數 據 及 其 相 應 標 籤 訓 練 模 型. 監 督 機 器 學 習 類 似 於 學 生 通 過 研 究 一 組 問 題 及 其 相 應 答 案 來 學 習 一門 學科。",  
       "[英] [related]  [Training a model] [from input data] [and its corresponding labels.]",
       "[Supervised machine learning] [is analogous to] [a student learning a subject],",
       "[by studying] [a set of questions] [and their corresponding answers.]",
