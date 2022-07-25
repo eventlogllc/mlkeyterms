15a81a2a697f086db995ecba821205a0ef306b24
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-07-24 21:46:25 -0700

    Update and rename unsupervised_machine_learning to unsupervised_machine_learning.js

--- tasks/tmp/unsupervised_machine_learning.js
+++ tasks/tmp/unsupervised_machine_learning.js
@@ -3,6 +3,8 @@
       "The most common use of unsupervised machine learning is to cluster data into groups of similar examples. For example, an unsupervised machine learning algorithm can cluster songs together based on various properties of the music. The resulting clusters can become an input to other machine learning algorithms (for example, to a music recommendation service). Clustering can be helpful in domains where true labels are hard to obtain. For example, in domains such as anti-abuse and fraud, clusters can help humans better understand the data.",
       "Another example of unsupervised machine learning is principal component analysis (PCA). For example, applying PCA on a dataset containing the contents of millions of shopping carts might reveal that shopping carts containing lemons frequently also contain antacids.",
       "Compare with supervised machine learning."
+      "[和] 教師なし機械学習はデータセット,（通常はラベルのないデータセット）内のパターンを見つけるためのモデルのトレーニングです.",
+      "[漢] 無 監督 機器 學習 是 訓練 模型 以 在 數據集 中 尋找 模式, 通常 是 未標記的 數據集.",
       "[英] [related] [Training a model] [to find patterns in a dataset], [typically an unlabeled dataset].",
       "[和] [関連] [訓練模型] [在數據集中尋找模式]，[通常是未標記的數據集]",	  
       " [訓練模型] [在數據集中尋找模式]，[通常是未標記的數據集]"
