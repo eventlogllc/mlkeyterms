9fa9053f6171e7760869c93720205198e73533fb
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-07-24 21:32:54 -0700

    Update precision.js

--- tasks/tmp/precision.js
+++ tasks/tmp/precision.js
@@ -1,6 +1,8 @@
 "what": [
       "A metric for classification models. Precision identifies the frequency with which a model was correct when predicting the positive class.",
       "precision = true_positives / ( true_positives + false_positives )",
+      "[和] 精度は分類モデルの指標です。 精度は、正のクラスを予測するときにモデルが正しかった頻度を識別します。",
+      "[漢] 精度是分類模型的一個指標。 精度識別模型在預測正類時正確的頻率。",
       "[英] [related]  [A metric] [for classification models]. [Precision identifies the frequency with which a model was correct] [when predicting the positive class] [precision] [=] [true_positives] [/] [ true_positives] + [false_positives]",
       "[和] [関連] [一種] [分類モデルのメトリック][精度はモデルが正しい頻度を識別します][ポジティブクラスを予測する場合][数学][精度][=][真陽性][/]（[真陽性] + [偽陽性]）,	    
       "[漢] [相關] [一種] [分類模型的指標] [精度識別模型正確的頻率] [在預測正類時] [精度] [=] ([真陽性] [/] [真陽性 + 假陽性])",
