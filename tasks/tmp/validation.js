9704e97594ac6c5ad11ff754c6b5d6eff9dbe50e
eventlogllc <100497634+eventlogllc@users.noreply.github.com>
2022-07-24 21:49:39 -0700

    Update validation.js

--- tasks/tmp/validation.js
+++ tasks/tmp/validation.js
@@ -4,6 +4,8 @@
       "Because the validation set is disjoint from the training set,",
       "validation helps ensure that the model\u2019s performance generalizes beyond the training set.",
       "Contrast with test set."
+      "[和] 検証は, トレーニングの一部として, 検証セットを使用して機械学習モデルの品質を評価するために使用されるプロセスです.",
+      "[漢] 驗證 是一個 過程，作為 訓練的 一部分，使用 驗證集 來 評估 機器學習 模型的 質量。",  
       "[英] [related]  [A process used as part of training] [to evaluate the quality of a machine learning model] [using the validation set.]",
       "[和] [関連] [トレーニングの一部として使用されるプロセス][機械学習モデルの品質を評価するため][検証セットを使用して]",
       "[漢] [相關] [用作訓練一部分的過程] [評估機器學習模型的質量] [使用驗證集。]", 
